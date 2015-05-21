4140e207ff8d6e7b905b4a0bed3b7da51c7fb08f
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2015-05-21 22:38:19 +0000

    update model browser

--- app/assets/javascripts/components/model_browser.js.jsx
+++ app/assets/javascripts/components/model_browser.js.jsx
@@ -1,46 +1,67 @@
-ModelNames = React.createClass({
+ModelAttributes = React.createClass({
   render: function() {
-    return <div id="names">
-      { model.attributes.map(function(att) {
-          }
+    return <div id="attributes">
+          {
+            model.attributes.map(
+              function(att) {
+                if (att.shown) {
+                  return <ModelAttribute key={att.name} attribute={att}/>;
+                }
+              })
+           }
         </div>
   }
 });
 
-ModelEditButton = React.createClass({
+ModelAttribute = React.createClass({
   render: function() {
-    if (this.props.edit_mode)
-      return <div className="inline">
-        <div id='cancel' onClick={ this.props.set_edit_mode.bind(null,false) }>&#x2717;</div>
-        <div id='approve' onClick={ this.props.submit_edit.bind(null) }>&#x2713;</div>
-      </div>
-    else
-      return <div id='edit' onClick={ this.props.set_edit_mode.bind(null,true) }>&#x270e;</div>
+    return <div className="attribute">
+            <div className="attribute_name">
+             { this.props.attribute.display_name }
+            </div>
+            <div className="attribute_value">
+             { this.props.attribute.display_name }
+            </div>
+           </div>
   }
 });
 
-ModelValues = React.createClass({
+ModelHeader = React.createClass({
   render: function() {
-    return <div id="values"/>
+    var button;
+    if (this.props.edit_mode)
+      button = 
+        <div className="inline">
+          <div id='cancel' onClick={ this.props.mode_handler.bind(null,'browse') }>&#x2717;</div>
+          <div id='approve' onClick={ this.props.mode_handler.bind(null,'submit') }>&#x2713;</div>
+        </div>
+    else
+      button = <div id='edit' onClick={ this.props.mode_handler.bind(null,'edit') }>&#x270e;</div>
+
+    return <div id="model_header">
+             { model.name }
+             { button }
+           </div>
   }
 });
+
 
 ModelBrowser = React.createClass({
   getInitialState: function() {
-    return { edit_mode: false }
+    return { mode: false }
   },
   submit_edit: function() {
     alert('ok');
+  },
+  handle_mode: function(mode) {
   },
   set_edit_mode: function(enable) {
     this.setState({ edit_mode: enable })
   },
   render: function() {
     return <div id="model_header">
-      { model.name }
-      <ModelEditButton submit_edit={ this.submit_edit} edit_mode={ this.state.edit_mode } set_edit_mode={ this.set_edit_mode }/>
-      <ModelNames/>
-      <ModelValues mode={ this.state.edit_mode }/>
+      <ModelHeader edit_mode={ this.state.edit_mode } mode_handler={ this.handle_mode }/>
+      <ModelAttributes edit_mode={ this.state.edit_mode }/>
     </div>
   }
 });
