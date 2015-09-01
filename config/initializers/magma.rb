f13c8c87bf064c2813c01b381d686b12edd4a16c
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2015-09-01 15:51:43 +0000

    use rails logger for sequel queries

--- config/initializers/magma.rb
+++ config/initializers/magma.rb
@@ -1,6 +1,8 @@
 require 'yaml'
+#require 'logger'
 
 Magma.instance.configure YAML.load(File.read("config/magma.yml"))
 
 Magma.instance.db.extension :connection_validator
 Magma.instance.db.pool.connection_validation_timeout = -1
+Magma.instance.db.loggers << Rails.logger
