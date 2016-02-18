8442233b2ce9ac0eed3963331178610c37f749ce
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2016-02-18 01:18:03 +0000

    table viewer help, such as it is

--- app/assets/javascripts/components/table.js.jsx
+++
@@ -1,5 +0,0 @@
-// this table viewer expects some basic inputs for the table.
-// 
-// records = an array of hashes with keys defined by column_name
-// columns = an array describing each column, with { name, type }
-
