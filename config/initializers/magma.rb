812a524889db333e0c32c57ce92be7d73a006082
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-01-27 18:45:58 +0000

    Cutoff Timur from Magma::Models

--- config/initializers/magma.rb
+++ config/initializers/magma.rb
@@ -4,9 +4,3 @@ require 'yaml'
 magma = Magma.instance
 
 magma.configure YAML.load(File.read("config/magma.yml"))
-
-magma.load_models
-
-magma.db.extension :connection_validator
-magma.db.pool.connection_validation_timeout = -1
-magma.db.loggers << Rails.logger
