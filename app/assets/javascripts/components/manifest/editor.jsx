a2b7a09b81369249936a819aaff06cb6317a1473
Darrell Abrau <darrellabrau@borealis-010060112074.ucsf.edu>
2017-03-24 14:33:34 -0700

    placeholder for manifest results

--- app/assets/javascripts/components/manifest/editor.jsx
+++ app/assets/javascripts/components/manifest/editor.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Titlebar from './title_bar'
 import ManifestElements from './manifest_elements'
+import JSONViewer from 'react-json-viewer'
 
-export default () => (
+const Editor = ({manifest}) => (
   <div className='manifest-container' >
     <Titlebar />
     <ManifestElements />
+    <JSONViewer json={manifest} />
   </div>
 )
+
+const mapStateToProps = (state) => {
+  const manifest = state.timur.manifests[state.manifestEditor.title]
+  return { 
+    manifest
+  }
+}
+
+export default connect(mapStateToProps)(Editor)
