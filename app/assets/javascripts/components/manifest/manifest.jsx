0a366b2023cb8f9af2b668f4f03fdbb14ebf4adc
Darrell Abrau <darrellabrau@PATH-3SCGTFL-LT.local>
2017-03-27 21:15:01 -0700

    Stubbed out tsv download

--- app/assets/javascripts/components/manifest/manifest.jsx
+++ app/assets/javascripts/components/manifest/manifest.jsx
@@ -1,62 +1,102 @@
 import React from 'react'
+import json2csv from 'json2csv'
+import downloadjs from 'downloadjs'
 
-const Matrix = ({col_names, row_names, rows}) => (
-  <table>
-    <thead>
-      <tr>
-        <th>row_names</th>
-        {col_names.map((columnName, index) => <th key={index}>{columnName}</th>)}
-      </tr>
-    </thead>
-    <tbody>
-      {rows.map((row, index) => (
-        <tr key={index}>
-          <td>{row_names[index]}</td>
-          { row.map((data, index) => <td key={index}>{data}</td>) }
+const matrixConversion = (data, fields) => {
+  return data.map(dataArr => {
+    return fields.reduce((acc, curr, index) => {
+      return {...acc, [curr]: dataArr[index]}
+    }, {})
+  })
+}
+
+const downloadCSV = (data, fields, fileName, matrixConversion = (data, fields) => data) => {
+  const jsonData = matrixConversion(data, fields)
+ 
+  try {
+    var result = json2csv({ data: jsonData, fields: fields, del: '\t'});
+    downloadjs(result, fileName+'.tsv', 'tsv');
+  } catch (err) {
+    // Errors are thrown for bad options, or if the data is empty and no fields are provided. 
+    // Be sure to provide fields if it is possible that your data array will be empty. 
+    console.error(err);
+  } 
+}
+
+const Matrix = ({col_names, row_names, rows, name}) => (
+  <div>
+    <h3>@{name}</h3>
+    <i className="fa fa-download" aria-hidden="true" onClick={() => downloadCSV(rows, col_names, name, matrixConversion)}></i>
+    <table>
+      <thead>
+        <tr>
+          <th>row_names</th>
+          {col_names.map((columnName, index) => <th key={index}>{columnName}</th>)}
         </tr>
-      ))}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {rows.map((row, index) => (
+          <tr key={index}>
+            <td>{row_names[index]}</td>
+            { row.map((data, index) => <td key={index}>{data}</td>) }
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 )
 
-const List = ({dataList}) => (
-  <table>
-    <thead>
-      <tr>
-        <th>label</th>
-        <th>value</th>
-      </tr>
-    </thead>
-    <tbody>
-      {dataList.map((data, index) => 
-        <tr key={index}>
-          <td>{data.label}</td>
-          <td>{data.value}</td>
+const List = ({dataList, name}) => (
+  <div>
+    <h3>@{name}</h3>
+    <i className="fa fa-download" aria-hidden="true" onClick={() => downloadCSV(dataList, ['label', 'value'], name)}></i>
+    <table>
+      <thead>
+        <tr>
+          <th>label</th>
+          <th>value</th>
         </tr>
-      )}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {dataList.map((data, index) => 
+          <tr key={index}>
+            <td>{data.label}</td>
+            <td>{data.value}</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  </div>
 )
 
 const isPrimitiveType = (value) => typeof value === 'string' || typeof value === 'number' || value === null || typeof value === 'undefined'
 
-const createManifestElement = (data) => {
+const createManifestElement = (name, data) => {
   if (isPrimitiveType(data)) {
-    return <div>{data}</div>
+    return ( 
+      <div>
+        <h3>@{name}</h3>
+        {data}
+      </div>
+    )
   } else if (Array.isArray(data) && data[0].label) {
     if (isPrimitiveType(data[0].value)) {
-      return <List dataList={data}/>
+      return <List dataList={data} name={name}/>
     } else {
-      console.log (data)
-      return data.map((elem, index) => (
-        <div key={index}>
-          <h3>{elem.label}</h3>
-          { createManifestElement(elem.value) }
+      return (
+        <div>
+          <h3>@{name}</h3>
+          {data.map((elem, index) => (
+            <div key={index}>
+              { createManifestElement(elem.label, elem.value) }
+            </div>
+          ))}
         </div>
-      ))
+      )
     }
   } else {
-    return <Matrix {...data.matrix} />
+    const props = { ...data.matrix, name }
+    return <Matrix {...props} />
   }
 } 
 
@@ -67,8 +107,7 @@ const manifest = (manifest) => {
     <div>
       {manifestElementList.map((elementName, index) => (
         <div key={index}>
-          <h3>@{elementName}</h3>
-          {createManifestElement(manifest[elementName]) }
+          {createManifestElement(elementName, manifest[elementName]) }
         </div>
       ))}
     </div>
@@ -76,10 +115,4 @@ const manifest = (manifest) => {
 }
 
 export default manifest
-
-
-
-
-    //   ))
-    // }     if (data[0] && isPrimitiveType(data[0].value)) {
 
