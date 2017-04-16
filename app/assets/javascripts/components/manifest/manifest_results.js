3286ddb447dd2b6c343d07e883b03fa17296608e
Darrell Abrau <darrellabrau@PATH-3SCGTFL-LT.local>
2017-04-15 21:33:57 -0700

    Tab nested results

--- app/assets/javascripts/components/manifest/manifest_results.js
+++ app/assets/javascripts/components/manifest/manifest_results.js
@@ -36,24 +36,28 @@ const ManifestResults = ({ results }) => {
 
 const isPrimitiveType = (value) => typeof value === 'string' || typeof value === 'number' || value === null || typeof value === 'undefined'
 
-export const Result = (name, data) => {
+export const Result = (name, data, nestLevel = 0) => {
   if (isPrimitiveType(data)) {
     return (
-      <div>
+      <div style={{ marginLeft: nestLevel * 5 }}>
         <span className='label'>@{name} - </span>
         {data}
       </div>
     )
   } else if (Array.isArray(data) && data[0].label) {
     if (isPrimitiveType(data[0].value)) {
-      return <List dataList={data} name={name}/>
+      return (
+        <div style={{marginLeft: nestLevel * 5 }}>
+          <List dataList={data} name={name}/>
+        </div>
+      )
     } else {
       return (
-        <div>
+        <div style={{marginLeft: nestLevel * 5 }}>
           <div>@{name}</div>
           {data.map((elem, index) => (
             <div key={index}>
-              { Result(elem.label, elem.value) }
+              { Result(elem.label, elem.value, nestLevel + 1) }
             </div>
           ))}
         </div>
@@ -61,7 +65,11 @@ export const Result = (name, data) => {
     }
   } else {
     const props = { ...data.matrix, name }
-    return <Matrix {...props} />
+    return (
+      <div style={{ marginLeft: nestLevel * 5 }}>
+        <Matrix {...props} />
+      </div>
+    )
   }
 }
 
