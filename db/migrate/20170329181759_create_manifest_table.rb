ba98c3759543f1854e82eeb6e0448970209222cd
Darrell Abrau <darrellabrau@PATH-3SCGTFL-LT.local>
2017-04-16 15:34:39 -0700

    manifest description nullable

--- db/migrate/20170329181759_create_manifest_table.rb
+++ db/migrate/20170329181759_create_manifest_table.rb
@@ -3,7 +3,7 @@ class CreateManifestTable < ActiveRecord::Migration
     create_table :manifests do |t|
       t.belongs_to :user, index: true, null: false
       t.string :name, null: false
-      t.string :description, null: false
+      t.string :description
       t.string :project, null: false
       t.string :access, null: false, index: true
       t.json :data, null: false
