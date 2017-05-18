b2974f5291a5b1e856cbe6fb25452974523aac9f
Darrell Abrau <darrellabrau@udp079387uds.ucsf.edu>
2017-05-18 12:03:26 -0700

    default to translate(0, number) when scale returns undefined

--- app/assets/javascripts/components/plots/xaxis.js.jsx
+++ app/assets/javascripts/components/plots/xaxis.js.jsx
@@ -11,36 +11,33 @@ const XAxis = ({
   label,
   ticks = scale.ticks(num_ticks),
   plotAreaWidth,
-}) => {
-  return (
-    <g className="axis">
-      <text textAnchor="middle" transform={ 'translate(' + (scale(xmin) + scale(xmax)) / 2 + ',' + (y + 35) + ')'}>
-        {label}
-      </text>
-      <line
-        y1={y}
-        x1={xmin ? scale(xmin) : 0}
-        y2={y}
-        x2={xmax ? scale(xmax) : plotAreaWidth}
-      />
-      {ticks.map((tick, i) => {
-        const x = typeof tick === 'string' ? scale(tick) + scale.rangeBand()/2 : scale(tick)
-        return (
-          <g key={i}>
-            <text textAnchor="end" transform={'translate(' + x + ',' + (y + tick_width + 10) + ') rotate(-45)'}>
-              {tickFormatter(ticks, scale)(tick)}
-            </text>
-            <line
-              y1={y}
-              x1={x}
-              y2={y + tick_width}
-              x2={x}
-            />
-          </g>
-        )
-      })}
-    </g>
-  )
-}
-
+}) => (
+  <g className="axis">
+    <text textAnchor="middle" transform={ 'translate(' + ((scale(xmin) || 0) + (scale(xmax) || 0)) / 2 + ',' + (y + 35) + ')'}>
+      {label}
+    </text>
+    <line
+      y1={y}
+      x1={xmin ? scale(xmin) : 0}
+      y2={y}
+      x2={xmax ? scale(xmax) : plotAreaWidth}
+    />
+    {ticks.map((tick, i) => {
+      const x = typeof tick === 'string' ? scale(tick) + scale.rangeBand()/2 : scale(tick)
+      return (
+        <g key={i}>
+          <text textAnchor="end" transform={'translate(' + x + ',' + (y + tick_width + 10) + ') rotate(-45)'}>
+            {tickFormatter(ticks, scale)(tick)}
+          </text>
+          <line
+            y1={y}
+            x1={x}
+            y2={y + tick_width}
+            x2={x}
+          />
+        </g>
+      )
+    })}
+  </g>
+)
 export default XAxis
