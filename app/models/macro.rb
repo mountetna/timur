48a0b3b77e5439160642f076323653e6bc6c9c1e
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-05-19 20:57:32 +0000

    rm log line

--- app/models/macro.rb
+++ app/models/macro.rb
@@ -5,7 +5,6 @@ class Macro
 
   def substitute args
     args.each.with_index.inject(@template) do |output, (arg, i)|
-      Rails.logger.info "#{arg} is a #{arg.class}"
       raise "Macro arguments must be Strings!" unless arg.is_a?(String)
       output.gsub(/\%#{i+1}/, %Q{ '#{arg}'})
     end
