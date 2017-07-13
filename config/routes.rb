5b2e59a2913a7ccb94f271f268384cbc4be84b1c
Jason Cater <jason.cater@ucsf.edu>
2017-07-13 14:24:39 -0700

    Organizing the routes by controller.

--- config/routes.rb
+++ config/routes.rb
@@ -4,30 +4,26 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  # welcome_controller.rb
   get 'static/:path'=> 'welcome#static', as: :static
   get 'login'=> 'welcome#login', as: :login
-  get 'noauth'=> 'welcome#noauth', as: :noauth
   get 'auth'=> 'welcome#auth', as: :auth
-  get 'auth_fail'=> 'welcome#auth_failure', as: :auth_failure
-  get 'auth_err'=> 'welcome#auth_error', as: :auth_error
+  get 'no_auth'=> 'welcome#no_auth', as: :no_auth
+  get 'auth_error'=> 'welcome#auth_error', as: :auth_error
 
-  # Routes for the main views - just browse for now
-  
-  get 'browse' => 'browse#index', as: :browse
-  get 'activity' => 'browse#activity', as: :activity
-
-  get 'browse/:model/*name' => 'browse#model', as: :browse_model, format: false
+  # browse_controller.rb
+  get 'browse/:project_name' => 'browse#index', as: :browse
+  get 'activity/:project_name' => 'browse#activity', as: :activity
+  get 'browse/:project_name/:model/*name' => 'browse#model', as: :browse_model, format: false
+  get 'map/:project_name' => 'browse#map', as: :map
   post 'browse/update' => 'browse#update', as: :update_model
+  post 'json/view' => 'browse#view_json', as: :view_json
 
   post 'json/records' => 'search#records_json', as: :records_json
   post 'json/query' => 'search#query_json', as: :query_json
-  post 'json/view' => 'browse#view_json', as: :view_json
-
   get 'search' => 'search#index', as: :search
   post 'search/table' => 'search#table_json', as: :table_json
   post 'search/tsv' => 'search#table_tsv', as: :table_tsv
-
-  get 'map' => 'browse#map', as: :map
 
   resources :manifests, only: [:index, :create, :update, :destroy]
 end
