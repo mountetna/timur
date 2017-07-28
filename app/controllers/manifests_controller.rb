df7511c200de674ef69f283612988167a9a0f8cb
Darrell Abrau <darrellabrau@udp079378uds.ucsf.edu>
2017-07-28 13:31:31 -0700

    Refactor manifests controller

--- app/controllers/manifests_controller.rb
+++ app/controllers/manifests_controller.rb
@@ -1,6 +1,4 @@
-class ManifestsController < ApplicationController
-  before_filter :authenticate
-
+class ManifestsController < ManifestsAPIController
   def index
     manifests = Manifest.where("user_id = ? OR access = ?", @current_user.id, "public").all
     manifest_json = manifests.map{ |manifest| manifest.to_json(@current_user) }
@@ -8,72 +6,44 @@ class ManifestsController < ApplicationController
   end
 
   def create
-    manifest = Manifest.new(manifest_params)
-    manifest.assign_attributes(:data => params[:data], :user_id => @current_user.id)
-
-    if manifest.save
-      render json: { :manifest => manifest.to_json(@current_user) }
-    else
-      error_response(manifest)
-    end
+    @manifest = @current_user.manifests.new(manifest_params)
+    save_manifest
   end
 
   def update
     return unless find_manifest(params[:id])
-    return unless authorize(@manifest)
+    return unless authorize
     @manifest.assign_attributes(manifest_params)
-    @manifest.assign_attributes(:data => params[:data])
-
-    if @manifest.save
-      render json: { :manifest => @manifest.to_json(@current_user) }
-    else
-      error_response(@manifest)
-    end 
+    save_manifest
   end
 
   def destroy
     return unless find_manifest(params[:id])
-    return unless authorize(@manifest)
-    if @manifest.destroy
-      render json: { :success => true }
+    return unless authorize
+    delete(@manifest)
+  end
+
+  private
+
+  def save_manifest
+    if @manifest.save
+      render json: { :manifest => @manifest.to_json(@current_user) }
     else
       error_response(@manifest)
     end
   end
 
-  private
-
-  def authenticate
-    if !current_user
-      render :json => { :errors => ["You must be logged in."] }, :status => 401 and return
-    end
-  end
-
-  def authorize(manifest)
-    authorized = manifest.can_edit?(@current_user)
-    if !authorized
-      render :json => { :errors => ["You must be the owner to update or delete this manifest."] }, :status => 403
-    end
-    authorized
-  end
-
-  def find_manifest(id)
-    @manifest = Manifest.find_by_id(id)
-    if @manifest.nil?
-      render :json => { :errors => ["Manifest with id: #{id} does not exist."] }, :status => 404
-    end
-    @manifest
-  end
-
-  def error_response(manifest)
-    render :json => { :errors => manifest.errors.full_messages }, :status => 422
-  end
-
   def manifest_params
     if @current_user.is_admin?
-      params.permit(:name, :description, :project, :access)
+      strong_params = params.permit(:name, :description, :project, :access)
     else
-      params.permit(:name, :description, :project)
+      strong_params = params.permit(:name, :description, :project)
     end
+
+    strong_params.tap do |whitelisted|
+      whitelisted[:data] = params[:data]
+    end
+
+    strong_params
   end
 end
