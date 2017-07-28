68cb318645402b9b5237cc8c00b5598d8ad94d6a
Darrell Abrau <darrellabrau@udp079378uds.ucsf.edu>
2017-07-28 13:31:44 -0700

    implement plot api

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,10 +1,46 @@
-class PlotsController < ApplicationController
+class PlotsController < ManifestsAPIController
   def create
+    @plot = @manifest.plots.new(plot_params)
+    save_plot
   end
 
   def update
+    return unless find_plot(params[:id])
+    save_plot
   end
-  
+
   def destroy
+    return unless find_plot(params[:id])
+    delete(@plot)
   end
+
+  private
+
+  def authorize
+    return unless find_manifest(params[:manifest_id])
+    return unless super
+  end
+
+  def find_plot(id)
+    @plot = Plot.find_by_id(id)
+    if @plot.nil?
+      render :json => { :errors => ["Plot with id: #{id} does not exist."] }, :status => 404
+    end
+    @plot
+  end
+
+  def save_plot
+    if @plot.save
+      render json: plot.as_json
+    else
+      render :json => { :errors => @plot.errors.full_messages }, :status => 422
+    end
+  end
+
+  def plot_params
+    params.permit(:name, :type).tap do |whitelisted|
+      whitelisted[:configuration] = params[:configuration]
+    end
+  end
+
 end
