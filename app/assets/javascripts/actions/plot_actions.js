1edaf23e030cbf83fc74ac8b068c5c25e40b7cbb
Darrell Abrau <darrellabrau@PEDS-C0TJ6Y1-DT.ucsf.edu>
2017-07-31 16:10:47 -0700

    Create and delete plot actions

--- app/assets/javascripts/actions/plot_actions.js
+++ app/assets/javascripts/actions/plot_actions.js
@@ -1,6 +1,22 @@
+import { destroyPlot } from '../api/plot_actions'
+
 // Add a plot to the store
 export const addPlot = (plot) => ({
   type: 'ADD_PLOT',
   plot
 })
 
+// remove a plot from the store
+const removePlot = (id) => ({
+  type: 'REMOVE_PLOT',
+  id
+})
+
+// Delete a plot from the database and the store
+export const deletePlot = (manifestId, plotId) =>
+  (dispatch) => {
+    destroyPlot(manifestId, plotId)
+      .then(() => {
+        dispatch(removePlot(plotId))
+      })
+  }
