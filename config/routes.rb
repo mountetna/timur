bda7f27a83a52b17522d78cc8585e29a81b6ede3
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-08-24 22:44:38 +0000

    Put project name first in routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,22 +12,22 @@ Rails.application.routes.draw do
   get 'auth_error'=> 'welcome#auth_error', as: :auth_error
 
   # browse_controller.rb
-  get 'browse/:project_name'=> 'browse#index', as: :browse
-  get 'activity/:project_name'=> 'browse#activity', as: :activity
-  get 'browse/:project_name/:model/*name'=> 'browse#model', as: :browse_model, format: false
-  get 'map/:project_name'=> 'browse#map', as: :map
-  post 'browse/update'=> 'browse#update', as: :update_model
-  post 'json/view'=> 'browse#view_json', as: :view_json
+  get ':project_name'=> 'browse#index', as: :browse
+  get ':project_name/activity'=> 'browse#activity', as: :activity
+  get ':project_name/browse/:model/*name'=> 'browse#model', as: :browse_model, format: false
+  get ':project_name/map'=> 'browse#map', as: :map
+  post ':project_name/update'=> 'browse#update', as: :update_model
+  post ':project_name/view'=> 'browse#view_json', as: :view_json
 
   # search_controller.rb
-  get 'search/:project_name'=> 'search#index', as: :search
+  get ':project_name/search'=> 'search#index', as: :search
   post 'json/records'=> 'search#records_json', as: :records_json
   post 'json/query'=> 'search#query_json', as: :query_json
   post 'search/table'=> 'search#table_json', as: :table_json
   post 'search/tsv'=> 'search#table_tsv', as: :table_tsv
 
   # manifest_controller.rb
-  get 'manifests/:project_name'=> 'manifests#index', as: :manifests
+  get ':project_name/manifests'=> 'manifests#index', as: :manifests
   post 'manifests/fetch'=> 'manifests#fetch', as: :manifests_fetch
   post 'manifests/create'=> 'manifests#create', as: :manifests_create
   post 'manifests/update/:id'=> 'manifests#update', as: :manifests_update
