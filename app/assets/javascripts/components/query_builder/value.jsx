67e73b4fe88679a5fb92fe14cb055cad0da1a11e
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-10-18 00:31:21 +0000

    fix predicates, prepare for inputs

--- app/assets/javascripts/components/query_builder/value.jsx
+++ app/assets/javascripts/components/query_builder/value.jsx
@@ -1,15 +1,33 @@
-import { Component } from 'react'
+import { Component } from 'react';
+import { selectVerbs } from '../../selectors/predicate';
+import { selectTemplate } from '../../selectors/magma';
+import Predicate from './predicate';
 
-export default class ValuePredicate extends Component {
-  constructor() {
-    super()
+class ValuePredicate extends Component {
+  getChild(verb, new_args) {
+    let { return_type, args } = verb;
+
+    if (return_type) return { type: 'terminal', return_type };
   }
 
   render() {
     // the model predicate has three terms, model, filters, and action
-    let { type } = this.props
-    return <div className='predicate'>
-      { type }
-      </div>
+    let { attribute_name, update, position, terms, verbs } = this.props;
+    let { args } = terms;
+    let special = () => [];
+    let child = this.getChild.bind(this);
+
+    return <Predicate
+      { ...{position, terms, update, verbs, special, args, child } }
+    />;
   }
 }
+
+export default connect(
+  (state,props) => {
+    let verbs =  selectVerbs(state,props.type);
+    let { attributes } = selectTemplate(state, props.terms.model_name);
+    let attribute = attributes[props.attribute_name];
+    return { verbs, attribute }
+  }
+)(ValuePredicate);
