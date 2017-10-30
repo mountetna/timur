5429000571877418eb291ebf3c97b785cbf805a1
Jason Cater <jason.cater@ucsf.edu>
2017-10-30 15:49:11 -0700

    Refactoring the main manifest view file.

--- app/assets/javascripts/components/manifest/manifest_view.jsx
+++ app/assets/javascripts/components/manifest/manifest_view.jsx
@@ -1,127 +1,190 @@
-import { Component } from 'react';
+// Framework libraries.
+import * as React from 'react';
+import * as ReactRedux from 'react-redux';
+
+// Class imports.
 import ManifestElement from './manifest_element';
 import ManifestPreview from './manifest_preview';
 import ButtonBar from '../button_bar';
-import ToggleSwitch from '../toggle_switch';
-import { requestConsignments } from '../../actions/consignment_actions';
-import { selectConsignment } from '../../selectors/consignment';
-import { manifestToReqPayload, deleteManifest, toggleEdit, copyManifest } from '../../actions/manifest_actions';
-import { getPlotsByIds } from '../../selectors/plot';
-import { selectPlot, toggleEditing as plotEdit } from '../../actions/plot_actions';
-import { plotIndexUrl } from '../../api/plots';
 
-// Shows a single manifest - it has two states, 'script', which
-// shows the manufest script, and 'output', which shows the
-// resulting data. Sends a request for a consignment when 'output'
-// is clicked if none exists.
-class ManifestView extends Component {
-  constructor() {
-    super()
-    this.state = { view_mode: 'script' }
+// Module imports.
+import {manifestToReqPayload, deleteManifest, toggleEdit, copyManifest} from '../../actions/manifest_actions';
+import {requestConsignments} from '../../actions/consignment_actions';
+import {selectConsignment} from '../../selectors/consignment';
+import {getPlotsByIds} from '../../selectors/plot';
+import {plotIndexUrl} from '../../api/plots';
+
+export class ManifestView extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {view_mode: 'script'};
   }
 
-  render() {
-    const { manifest, consignment, deleteManifest, toggleEdit, copyManifest, requestConsignments } = this.props
-    const { is_editable, name } = manifest
+  renderButtonBar(){
 
-    const elements =  manifest.data.elements || []
-
-    const manifestElements = elements.map((element, i) => {
-      let elementResult
-      if (consignment) {
-        if (consignment[element.name]) {
-          elementResult = consignment[element.name]
-        } else if (consignment && !consignment[element.name]) {
-          elementResult = ''
-        } 
-      } else {
-        elementResult = ''
-      }
-      const props = { ...element, result: elementResult, view_mode: this.state.view_mode }
-      return (
-        <li key={i}>
-          <ManifestElement {...props}/>
-        </li>
-      )
-    })
+    let {manifest, toggleEdit, deleteManifest, copyManifest} = this.props;
+    let {is_editable} = manifest;
 
     let buttons = [
+      {
+        click: ()=>copyManifest(manifest),
+        icon: 'files-o',
+        label: 'copy'
+      },
       is_editable && {
         click: toggleEdit,
         icon: 'pencil-square-o',
         label: 'edit'
       },
-
-      this.state.view_mode == 'output' && is_editable && {
-        click: () => location.href = plotIndexUrl({ manifest_id: manifest.id, is_editing: true }),
-        icon: 'line-chart',
-        label: 'plot'
-      },
-
-      {
-        click: () => copyManifest(manifest),
-        icon: 'files-o',
-        label: 'copy'
-      },
-
       is_editable && {
-        click: () => deleteManifest(manifest.id),
+        click: ()=>deleteManifest(manifest.id),
         icon: 'trash-o',
         label: 'delete'
+      },
+      is_editable && this.state.view_mode == 'output' && {
+        click: ()=>{
+          location.href = plotIndexUrl({
+            manifest_id: manifest.id,
+            is_editing: true
+          });
+        },
+        icon: 'line-chart',
+        label: 'plot'
       }
-    ].filter(_=>_)
+    ].filter(button=>button);
 
-    return (
+    return <ButtonBar className='actions' buttons={buttons} />
+  }
+
+  renderToggleSwitch(){
+    let {manifest, consignment, requestConsignments} = this.props;
+    let query_btn_props = {
+      className: 'manifest-query-button',
+      onClick: (evt)=>{
+        this.setState({view_mode: 'output'});
+        if(!consignment) requestConsignments([manifestToReqPayload(manifest)]);
+      },
+      disabled: (this.state.view_mode == 'output') ? 'disabled' : ''
+    };
+
+    let script_btn_props = {
+      className: 'manifest-query-button',
+      onClick: (evt)=>{
+        this.setState({view_mode: 'script'});
+      },
+      disabled: (this.state.view_mode == 'script') ? 'disabled' : ''
+    };
+
+    return(
+      <span>
+
+        <button {...query_btn_props}>
+
+          {'Run Query'}
+        </button>
+        <button {...script_btn_props}>
+
+          {'Show Script'}
+        </button>
+      </span>
+    );
+  }
+
+  renderManifestElements(){
+    let {manifest, consignment} = this.props;
+
+    let manifest_elements = manifest.data.elements || [];
+    manifest_elements = manifest_elements.map((element, index)=>{
+
+      let element_result = '';
+      if(consignment && consignment[element.name]){
+        element_result = consignment[element.name];
+      }
+
+      let props = {
+        ...element,
+        result: element_result,
+        view_mode: this.state.view_mode
+      };
+
+      return(
+        <li key={index}>
+
+          <ManifestElement {...props} />
+        </li>
+      );
+    });
+
+    return <ol>{manifest_elements}</ol>;
+  }
+
+  renderPlots(){
+    if(this.state.view_mode != 'output') return null;
+
+    let {manifest} = this.props;
+    let plots = this.props.plots.map((plot, index)=>{
+      return(
+        <li key={plot.id}>
+
+          <a href={plotIndexUrl({manifest_id: manifest.id, id: plot.id})}>
+
+            {plot.name}
+          </a>
+        </li>
+      );
+    });
+
+    return(
+      <div>
+
+        <div>{'Plots: '}</div>
+        <ul>{plots}</ul>
+      </div>
+    );
+  }
+
+  render(){
+    return(
       <div className='manifest'>
+
         <div className='manifest-elements'>
-          <ButtonBar className='actions' buttons={ buttons }/>
-          <ManifestPreview {...manifest} />
-          <ToggleSwitch 
-            id="view_mode_switch"
-            caption="Show"
-            onChange={ (view_mode) => {
-              this.setState({ view_mode })
-              if (view_mode == 'output' && !consignment) {
-                if (!consignment) requestConsignments([manifestToReqPayload(manifest)])
-              }
-            } }
-            selected={this.state.view_mode}
-            values={[ 'script', 'output' ]}
-          />
-          <ol>
-            {manifestElements}
-          </ol>
-          {this.state.view_mode === 'output' &&
-            <div>
-              <div>{'Plots: '}</div>
-              <ul>
-                {this.props.plots.map(plot => (
-                  <li key={plot.id}>
-                    <a href={plotIndexUrl({ manifest_id: manifest.id, id: plot.id })}>
-                      {plot.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          }
+
+          {this.renderButtonBar()}
+          <ManifestPreview {...this.props.manifest} />
+          {this.renderToggleSwitch()}
+          {this.renderManifestElements()}
+          {this.renderPlots()}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default connect(
-  (state, props) => ({
-    consignment: selectConsignment(state, props.manifest.name),
-    plots: getPlotsByIds(state, props.manifest.plotIds || [])
-  }),
-  {
-    deleteManifest,
-    copyManifest,
-    toggleEdit,
-    requestConsignments,
-    selectPlot,
-    plotEdit
-  }
-)(ManifestView)
+const mapStateToProps = (state = {}, own_props)=>{
+  return {
+    consignment: selectConsignment(state, own_props.manifest.name),
+    plots: getPlotsByIds(state, own_props.manifest.plotIds || [])
+  };
+};
+
+const mapDispatchToProps = (dispatch, own_props)=>{
+  return {
+    copyManifest: (manifest_record)=>{
+      dispatch(copyManifest(manifest_record));
+    },
+    deleteManifest: (manifest_id)=>{
+      dispatch(deleteManifest(manifest_id));
+    },
+    toggleEdit: (manifest_action)=>{
+      dispatch(toggleEdit(manifest_action));
+    },
+    requestConsignments: (manifests)=>{
+      dispatch(requestConsignments(manifests));
+    }
+  };
+};
+
+export default ReactRedux.connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ManifestView);
