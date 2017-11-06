97fbf49a823fb0b9890acca1d576e623ba725f42
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-11-06 21:36:51 +0000

    build image assets in webpack using file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, 'app/assets/javascripts'),
+  context: path.resolve(__dirname, 'app/assets'),
   resolve: {
-    extensions: [ '.js', '.jsx' ]
+    extensions: [ '.js', '.jsx', '.scss', '.png', '.jpg', '.jpeg', '.svg' ],
+    alias: {
+      'prism': path.join(__dirname, 'node_modules/prismjs/themes'),
+      'font-awesome': path.join(__dirname, 'node_modules/font-awesome')
+    }
   },
-  entry: [
-    './timur.jsx'
-  ],
+  entry: ['./javascripts/timur.jsx', './stylesheets/application.scss' ],
+  output: {
+    filename: 'public/js/timur.bundle.js',
+    path: __dirname
+  },
   module: {
-    loaders: [
+    rules: [
       {
-        loader: "babel-loader",
+        loader: 'babel-loader',
 
         // Skip any files outside of your project's `src` directory
         include: [
-          path.resolve(__dirname, "app/assets/javascripts"),
+          path.resolve(__dirname, 'app/assets/javascripts'),
         ],
 
         // Only run `.js` and `.jsx` files through Babel
@@ -26,10 +33,51 @@ module.exports = {
           presets: ['es2015', 'stage-0', 'react'],
         }
       },
+
+      {
+        test: /\.(jpe?g|png|svg)$/i,
+
+        include: [
+          path.resolve(__dirname, 'app/assets/images'),
+        ],
+
+        loader: 'file-loader',
+
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'public/images/',
+          publicPath: function(url) { return url.replace(/public/,'') }
+        }
+      },
+
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*$|$)/,
+
+        include: [
+          path.resolve(__dirname, 'node_modules/font-awesome'),
+        ],
+
+        loader: 'file-loader',
+        
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'public/fonts/',
+          publicPath: function(url) { return url.replace(/public/,'') }
+        }
+      },
+
+      {
+        // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
     ]
   },
-  output: {
-    filename: "public/js/timur.bundle.js",
-    path: __dirname
-  }
-}
+  plugins: [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'public/css/timur.bundle.css',
+      allChunks: true,
+    }),
+  ],
+};
