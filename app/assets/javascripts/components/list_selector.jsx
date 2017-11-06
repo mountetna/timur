2086f26e44dd6bda266d4004ac90423029bc0cb9
Jason Cater <jason.cater@ucsf.edu>
2017-11-06 11:26:44 -0800

    Updating the list-selector component.

--- app/assets/javascripts/components/list_selector.jsx
+++ app/assets/javascripts/components/list_selector.jsx
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+// Framework libraries.
+import * as React from 'react';
 
 /*
  * This component presents a list of items to select, possibly divided into
@@ -14,72 +15,65 @@ import { Component } from 'react';
  * If there is a 'sections', 'items' is ignored.
  */
 
-export default class ListSelector extends Component {
-  listSection(section_name, section) {
-    return (
-      <div className='section'>
-        {
-          section_name && <span className='title'>{ section_name }</span> 
+export default class ListSelector extends React.Component{
+
+  listSelection({id, title, name}, index){
+      var btn_props = {
+        'className': 'list-selection',
+        'title': title,
+        'key': 'list-'+id,
+        'onClick': ()=>{
+          this.props.select(id);
         }
-        <ol>
-          {
-            section.map(item => this.listSelection(item))
-          }
-        </ol>
+      };
+
+    return <button {...btn_props}>{name}</button>;
+  }
+
+  listSection(section_name, section, id){
+    return (
+      <div className='list-selector-panel' key={id}>
+
+        {section_name && <div className='list-selector-header'>{section_name}</div>}
+        {section.map((item, index) => this.listSelection(item, index))}
       </div>
     );
   }
 
-  listSelection({ id, title, name}) {
-    return (
-      <li key={id}>
-        <div className='selection'>
-          <a href='#' 
-            onClick={
-              () => this.props.select(id) 
-            }
-            title={ title }>
-            <span className='name'>{name}</span>
-          </a>
-        </div>
-      </li>
-    );
-  }
-
-  renderList() {
-    let { sections, items } = this.props;
-
-    if (sections) {
-      return Object.keys(sections).map(section_name => this.listSection(section_name, sections[section_name]));
-    } else if (items) {
+  renderList(){
+    let {sections, items} = this.props;
+    if(sections){
+      return Object.keys(sections).map((section_name, index)=>{
+        return this.listSection(section_name, sections[section_name], index);
+      });
+    }
+    else if(items){
       return this.listSection(null, items);
     }
     return null;
   }
 
-  renderCreate() {
-    let { name, create } = this.props;
-
-    if (!create) return null;
+  renderCreate(){
+    let {name, create} = this.props;
+    if(!create) return null;
 
     return (
-      <a href='#' onClick={ () => create() } className='new'>
+      <button onClick={()=>create()} className='list-selector-new-btn'>
+
         <i className='fa fa-plus' aria-hidden='true' />
-        New { name }
-      </a>
+        {` NEW ${name}`}
+      </button>
     );
   }
 
   render() {
     return (
-      <div className='list-selector'>
-        {
-          this.renderCreate(name)
-        }
-        <div>
-          {
-            this.renderList()
-          }
+      <div className='list-selector-group'>
+
+        {this.renderCreate(name)}
+        <div className='list-selector-panel'>
+
+          {this.renderList()}
         </div>
       </div>
     );
