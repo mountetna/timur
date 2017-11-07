c1d875f461c931435897880f575a662a30525cb6
Jason Cater <jason.cater@ucsf.edu>
2017-11-07 11:27:01 -0800

    Cleaning out an extra newline.

--- app/assets/javascripts/selectors/consignment.js
+++ app/assets/javascripts/selectors/consignment.js
@@ -1,7 +1,6 @@
 import { createSelector } from 'reselect';
 import Consignment from '../models/consignment';
 
-
 const selectConsignmentData = (state, manifest_name) => state.consignments[manifest_name];
 
 // cached construction of a Consignment object from a given consignment JSON
