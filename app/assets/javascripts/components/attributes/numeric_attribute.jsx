c469d2959476435a04ff8aaa8a934d4f04c91de7
Darrell Abrau <darrellabrau@otp1248325ots.ucsf.edu>
2017-11-09 19:09:45 -0800

    Revert "Use higher order component to inject input to attribute"
    This reverts commit 5fda7a8fb1827571394ba8a474dd5e76fcb65c6c.

--- app/assets/javascripts/components/attributes/numeric_attribute.jsx
+++ app/assets/javascripts/components/attributes/numeric_attribute.jsx
@@ -4,44 +4,40 @@ import { reviseDocument } from '../../actions/magma_actions';
 import React, { Component } from 'react';
 import { IntegerInput, FloatInput } from '../inputs/numeric_input';
 
-const NumericAttribute = (Input) => {
-  return class extends Component {
-    render() {
-      let {mode, value} = this.props;
+class NumericAttribute extends Component {
+  render() {
+    let { mode, value } = this.props;
 
-      return <div className='value'>
-        {
-          mode == 'edit' ? this.renderInput() : value
-        }
-      </div>;
-    }
+    return <div className='value'>
+      {
+        mode == 'edit' ? this.renderInput() : value
+      }
+    </div>;
+  }
 
-    onChange(value) {
-      let {document, template, attribute, reviseDocument} = this.props;
+  onChange(value) {
+    let { document, template, attribute, reviseDocument } = this.props;
 
-      reviseDocument(
-        document, template, attribute, value
-      );
-    }
+    reviseDocument(
+      document, template, attribute, value
+    );
+  }
 
-    renderInput() {
-      let {revision, attribute} = this.props;
+  renderInput() {
+    let { numericInput, revision, attribute } = this.props;
 
-      return <Input
+    return <numericInput 
         onChange={ this.onChange.bind(this) }
         className='full_text'
         placeholder={attribute.placeholder}
-        defaultValue={ revision }/>;
-    }
-  };
-};
+        defaultValue={ revision } />;
+  }
+}
 
-export const IntegerAttribute = connect(
+NumericAttribute = connect(
   null,
   { reviseDocument }
-)(NumericAttribute(IntegerInput));
+)(NumericAttribute);
 
-export const FloatAttribute = connect(
-  null,
-  { reviseDocument }
-)(NumericAttribute(FloatInput));
+export const IntegerAttribute = (props) => <NumericAttribute {...props } numericInput={ IntegerInput }/>;
+export const FloatAttribute = (props) => <NumericAttribute {...props } numericInput={ FloatInput }/>;
