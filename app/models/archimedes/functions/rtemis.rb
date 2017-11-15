cd5cb3464ea77134b70c03edb46cf0840b8754da
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-11-15 05:56:13 +0000

    add correlation

--- app/models/archimedes/functions/rtemis.rb
+++ app/models/archimedes/functions/rtemis.rb
@@ -1,6 +1,6 @@
 module Archimedes
   class RtemisFunctions < Archimedes::Function
-    FUNCTIONS = [ :diff_exp, :pca, :center, :scale, :transpose, :sd ]
+    FUNCTIONS = [ :diff_exp, :pca, :center, :scale, :transpose, :sd, :correlation ]
 
     def self.is_func? function_name
       FUNCTIONS.include?(function_name.to_sym)
