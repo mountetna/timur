1e75ed27a9a99254584b6103fce0dec29024b6f5
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-11-15 00:33:49 +0000

    modify brace style

--- app/assets/javascripts/components/table_viewer.jsx
+++ app/assets/javascripts/components/table_viewer.jsx
@@ -8,20 +8,18 @@ import Pager from './pager';
 import Help from './help';
 import AttributeViewer from './attributes/attribute_viewer';
 
-const TableColumn = (template, document) => (att_name) => {
-  return(
-    <div className="table_data" key={att_name}>
+const TableColumn = (template, document) => (att_name) => (
+    <div className='table_data' key={att_name}>
       <AttributeViewer 
         template={ template }
         document={ document }
         value={ document[ att_name ] }
         attribute={ template.attributes[att_name] }/>
     </div>
-  )
-};
+);
 
 const TableRow = (template, documents, attribute_names) => (record_name) => (
-  <div key={ record_name } className="table_row">
+  <div key={ record_name } className='table_row'>
   {
     attribute_names.map(TableColumn(template, documents[record_name]))
   }
@@ -37,11 +35,11 @@ class TableViewer extends React.Component {
         current_page={ current_page + 1 }
         setPage={ setPage } >
         <div className='search'>&#x2315;</div>
-        <input className="filter" type="text" onChange={ (e) => onFilter(e.target.value) }/>
-        <input className="export" type="button" onClick={ () => requestTSV(model_name, record_names) } value={"\u21af TSV"}/>
-        <Help info="table_viewer"/>
+        <input className='filter' type='text' onChange={ (e) => onFilter(e.target.value) }/>
+        <input className='export' type='button' onClick={ () => requestTSV(model_name, record_names) } value={'\u21af TSV'}/>
+        <Help info='table_viewer'/>
       </Pager>
-    )
+    );
   }
 
   renderRecords() {
@@ -60,10 +58,10 @@ class TableViewer extends React.Component {
     let { attribute_names } = this.props;
 
     return(
-      <div className="table_row">
+      <div className='table_row'>
       {
         attribute_names.map((att_name,i) => 
-          <div key={i} className="table_header">{ att_name }</div>
+          <div key={i} className='table_header'>{ att_name }</div>
         )
       }
       </div>
@@ -71,31 +69,26 @@ class TableViewer extends React.Component {
   }
 
   render() {
-    console.log("Rendering "+this.props.model_name);
-    let { template, documents, record_names, page_size, current_page } = this.props;
+    let { template } = this.props;
 
     if (!template) return <div/>;
 
-    return <div className="table">
-      {
-        this.renderPager()
-      }
-      {
-        this.renderHeader()
-      }
-      {
-        this.renderRecords()
-      }
+    return(
+      <div className='table'>
+        { this.renderPager() }
+        { this.renderHeader() }
+        { this.renderRecords() }
       </div>
+    );
   }
 }
 
 export default connect(
   function(state,props) {
-    var magma = new Magma(state);
-    var template = magma.template(props.model_name);
-    var documents = magma.documents( props.model_name, props.record_names, props.filter );
-    var record_names = Object.keys(documents).sort();
+    let magma = new Magma(state);
+    let template = magma.template(props.model_name);
+    let documents = magma.documents( props.model_name, props.record_names, props.filter );
+    let record_names = Object.keys(documents).sort();
     let pages = Math.ceil(record_names.length / props.page_size);
     let attribute_names = template ? Object.keys(template.attributes).filter(
       att_name => template.attributes[att_name].shown && template.attributes[att_name].attribute_class != 'Magma::TableAttribute'
