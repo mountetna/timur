b038af905b806d86b9088a52e3a57c120e4621c4
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-11-15 22:02:52 +0000

    rm unused diff_exp.rb

--- app/models/archimedes/functions/diff_exp.rb
+++
@@ -1,9 +0,0 @@
-module Archimedes
-  class DiffExp < Archimedes::Function
-    def call
-      matrix, p_value, group_one, group_two = @args
-
-      Rtemis.instance.call(:diff_exp, matrix, p_value, group_one, group_two)
-    end
-  end
-end
