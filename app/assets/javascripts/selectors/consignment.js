f0bdc938d3c24b88d4418c291e139d15ef918d07
Jason Cater <jasondcater@gmail.com>
2017-11-20 10:59:09 -0800

    Sorting the consignment by manifest id.

--- app/assets/javascripts/selectors/consignment.js
+++ app/assets/javascripts/selectors/consignment.js
@@ -1,7 +1,7 @@
 import { createSelector } from 'reselect';
 import Consignment from '../models/consignment';
 
-const selectConsignmentData = (state, manifest_name) => state.consignments[manifest_name];
+const selectConsignmentData = (state, manifest_id) => state.consignments[manifest_id];
 
 // cached construction of a Consignment object from a given consignment JSON
 export const selectConsignment = createSelector(
