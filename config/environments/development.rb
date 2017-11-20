96b8992694732c426bcb0af5f8d7c9c92b052aae
Jason Cater <jasondcater@gmail.com>
2017-11-20 10:57:40 -0800

    Adding an option to serve the webpack generated UI.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,4 +43,6 @@ Rails.application.configure do
   config.pythia_url = "http://localhost:5000/pythia/"
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.serve_static_files = true
 end
