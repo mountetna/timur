40fb1e307cd8d5cd3302748674d116398acbc44b
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-12-03 00:09:35 +0000

    enable wilcox test

--- app/models/archimedes/functions/rtemis.rb
+++ app/models/archimedes/functions/rtemis.rb
@@ -1,6 +1,6 @@
 module Archimedes
   class RtemisFunctions < Archimedes::Function
-    FUNCTIONS = [ :diff_exp, :pca, :center, :scale, :transpose, :sd, :correlation ]
+    FUNCTIONS = [ :diff_exp, :pca, :center, :scale, :transpose, :sd, :correlation, :wilcox ]
 
     def self.is_func? function_name
       FUNCTIONS.include?(function_name.to_sym)
