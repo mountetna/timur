8189be6725c68cbd384d8911b9f78f0d473eb6c0
Jason Cater <jason.cater@ucsf.edu>
2017-12-08 13:08:15 -0800

    Adding an ability to get the consigment ids.

--- app/assets/javascripts/selectors/consignment.js
+++ app/assets/javascripts/selectors/consignment.js
@@ -8,3 +8,14 @@ export const selectConsignment = createSelector(
   selectConsignmentData,
   (consignment) => (consignment ? new Consignment(consignment) : null)
 );
+
+/*
+ * Get the consignment id's (which should be the same as the manifest ids). We 
+ * can use this to tell which consignments have been loaded. This way we don't
+ * fetch data that we alread have cached.
+ */
+export const getLoadedConsignmentIds = (state)=>{
+  let ids = [];
+  for(let key in state.consignments) ids.push(key);
+  return ids;
+};
