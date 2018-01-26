25bd34fd7853547c05b738c9d3ce6001cd3f09cb
Jason Cater <jason.cater@ucsf.edu>
2018-01-26 12:00:40 -0800

    Adding md5sums of the manifests for client side consignment cacheing.

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 class Manifest < ActiveRecord::Base
   belongs_to :user
   has_many :plots, dependent: :destroy
@@ -6,7 +8,13 @@ class Manifest < ActiveRecord::Base
   validates :name, presence: true
   validates :project, presence: true
   validates :data, presence: true
-  validates :access, inclusion: {in: %w(public private view), message: "%{value} is not a valid access"}
+
+  validates :access, {
+    inclusion: {
+      in: %w(public private view),
+      message: "%{value} is not a valid access"
+    }
+  }
 
   def is_public?
     access == 'public'
@@ -17,14 +25,35 @@ class Manifest < ActiveRecord::Base
   end
 
   def to_json(user, project_name)
-    json = self.as_json(except: [:user_id], include: [{user: {only: :name }}])
+
+    # Add some basic ownership data to the manfiest.
+    json = self.as_json({
+      except: [
+        :user_id
+      ],
+      include: [
+        user: {
+          only: [:id, :name, :email]
+        }
+      ]
+    })
+
+    # Check if the current user can edit this manifest.
     json['is_editable'] =  can_edit?(user, project_name)
-    json
+
+    # These md5sums will be used for consignment and manifest cache checking. We
+    # must make sure that the md5sums are NOT part of the hashes themselves.
+    md5sum = Digest::MD5.hexdigest(json.to_json)
+    md5sum_data = Digest::MD5.hexdigest(json['data'].to_json)
+
+    # Add the hashes to the manifest.
+    json['md5sum'] = md5sum
+    json['md5sum_data'] = md5sum_data
+
+    return json
   end
 
   def set_defaults
-    if self.new_record?
-      self.access ||= 'private'
-    end
+    if self.new_record? then self.access ||= 'private' end
   end
 end
