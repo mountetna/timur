77e830b80f467f1c5290ebbf27fb92061760728b
Eleanor Mark <eleanormark@gmail.com>
2018-02-09 23:23:09 -0800

    Add editing function and style to settings viewpage

--- app/assets/javascripts/components/settings/settings_view.jsx
+++ app/assets/javascripts/components/settings/settings_view.jsx
@@ -4,6 +4,7 @@ import * as ReactRedux from 'react-redux';
 import ListMenu from '../list_menu';
 import ButtonBar from '../button_bar';
 import {requestViewSettings} from '../../actions/timur_actions';
+import { selectConsignment } from '../../selectors/consignment_selector';
 
 export class SettingsView extends React.Component{
   constructor(props){
@@ -11,95 +12,156 @@ export class SettingsView extends React.Component{
 
     this.state = {
       selected_model: null,
-      is_editing: false
+      selected_view_settings: '',
+      is_editing: false,
+      page_status: ''
     };
   }
-
+  
   componentDidMount(){
     this.props.fetchViewSettings();
   }
 
-  updateField(field_name){
+  cloneView(selected_model){
+    
+    return JSON.parse(JSON.stringify(this.props.views.views[selected_model]));
+  }
+
+  updateField(property){
     return (event)=>{
-      let manifest = this.state.manifest;
-      manifest[field_name] = event.target.value;
-      this.setState({manifest});
-    }
+
+      let views = this.state.selected_view_settings;
+      switch(property) {
+        case 'tabs': 
+          views[property] = JSON.parse(event.target.value);
+          break;
+        case 'model_name': 
+          views[property] = event.target.value;
+          break;
+        default:
+          return;
+      }
+      this.setState({selected_view_settings: views});
+    };
+  }
+
+  editableButtons(){
+
+    let {} = this.props;
+    return [
+      {
+        click: () => {},
+        icon: 'trash-o',
+        label: ' DELETE'
+      },
+      {
+        click: this.toggleEdit.bind(this),
+        icon: 'pencil-square-o',
+        label: ' EDIT'
+      }
+    ].filter(button=>button);
+  }
+
+  editingButtons(){
+    return [
+      {
+        click: () => {},
+        icon: 'floppy-o',
+        label: ' SAVE'
+      },
+      {
+        click: this.cancelEdit.bind(this),
+        icon: 'ban',
+        label: ' CANCEL'
+      }
+    ].filter(button=>button);
+  }
+
+  cancelEdit(){
+
+    // Reset the vuew
+    this.setState({
+      selected_view_settings: this.cloneView(this.state.selected_model),
+      page_status: ''
+    });
+
+    // Turn of the editing mode.
+    this.toggleEdit();
+  }
+
+  toggleEdit(){
+    this.setState({
+      is_editing: (!this.state.is_editing),
+      page_status: (!this.state.is_editing) ? 'EDITING' : ''
+    });
   }
 
   // This is the main panel in which we inspect view code.
-  renderViewInspector(model_name){
+  renderViewInspector(){
 
-    if(!this.props.views) return null;
-    let view_code = this.props.views.views[model_name];
+    let disabled = (!this.state.is_editing) ? 'disabled' : '';
+    let tabs_code = this.state.selected_view_settings.tabs;
+    let tabs_attributes = {
+
+      className: `${disabled} settings-view-tab-group`,
+      value: JSON.stringify(tabs_code, null, 2),
+      onChange: this.updateField('tabs'),
+      disabled
+    };
     return(
-      <div className='view-setting-inpector-group'>
+      <div className='settings-view-inspector-group'>
         
-        {this.renderTabEntries(view_code)}
+        <textarea {...tabs_attributes}></textarea>
       </div>
     );
   }
 
-  // This renders individual tabs for a project in the inpsector.
-  renderTabEntries(view_code){
+  rendeRightColumnGroup() {
 
-    let tabs = [];
-    let index = 0;
+    let { selected_view_settings, is_editing, page_status } = this.state;
+    let disabled = (!is_editing) ? 'disabled' : '';
 
-    for(let tab_name in view_code.tabs){
+    let input_props = {
+      className: `${disabled} settings-view-model-title-input`,
+      onChange: this.updateField('model_name'),
+      value: selected_view_settings.model_name,
+      type: 'text',
+      disabled
+    };
 
-      let tab = (
-        <div className='view-setting-tab-group' key={tab_name+'-'+index}>
+    let buttons;
+    if(is_editing){
+      buttons = this.editingButtons();
+    }
+    else{
+      buttons = this.editableButtons();
+    }
+    
+    return(
+      <div className='settings-view-elements'>
+        <div className='settings-view-form-header'>
 
-          <div className='view-setting-tab-header'>
+          <div className='settings-view-form-title'>
 
-            {'TAB NAME : '+tab_name}
+            {'NAME: '}
+            <input {...input_props} />
+            <ButtonBar className='settings-view-action-btn-group' buttons={buttons} />
+          
+            <span style={{float: 'right'}}>
+
+              {page_status}
+            </span>
           </div>
-          {this.renderTabPanes(view_code.tabs[tab_name])}
+          <br />
         </div>
-      );
-      tabs.push(tab);
-
-      ++index;
-    }
-
-    return tabs;
+        { this.renderViewInspector() }
+      </div>
+    );
   }
-
-    // This renders the pane code for a tab.
-    renderTabPanes(pane_code){
-
-      let panes = [];
-      let index = 0;
-  
-      for(let pane_name in pane_code.panes){
-  
-        let pane_props = {
-          className: 'view-setting-pane',
-          value: JSON.stringify(pane_code.panes[pane_name], null, 2),
-          onChange: (evt)=>{
-            console.log(evt);
-          }
-        };
-  
-        panes.push(
-          <div className='view-setting-pane-group' key={pane_name+'-'+index}>
-            
-            <div className='view-setting-pane-header'>
-  
-              {'PANE NAME : '+pane_name}
-            </div>
-            <textarea {...pane_props}></textarea>
-          </div>
-        );
-      }
-      return panes;
-    }
 
   render(){
     let list_menu_props;
-    let { is_editing } = this.state;
-    let disabled = (!is_editing) ? 'disabled' : '';
+    let { selected_model } = this.state;
   
     if(this.props.views) {
 
@@ -114,46 +176,25 @@ export class SettingsView extends React.Component{
         name: 'Settings',
         create: function() {},
         select: (id)=>{ 
-          this.setState({selected_model: view_items[id].name});
+          this.setState({
+            selected_model: view_items[id].name,
+            selected_view_settings: this.cloneView(view_items[id].name)
+          });
         },
         items: view_items,
       };
     }
 
-    let input_props = {
-      className: `${disabled} manifest-form-title-input`,
-      value: this.state.selected_model,
-      type: 'text',
-      disabled
-    };
-
     return(
-      <div className='settings-group'>
-        <div className='manifest-form-header'>
-
-          <div className='manifest-form-title'>
-
-            { 'NAME: '}
-            <input {...input_props} />
-            
-            <span style={{float: 'right'}}>
-
-              {'page_status'}
-            </span>
-          </div>
-          <br />
-        </div>
+      <div className='settings-view-group'>
 
         <div className='left-column-group'>
 
-          <div className='left-column-menu-group'>
-
-            { list_menu_props && <ListMenu {...list_menu_props} /> }
-          </div>
+          { list_menu_props && <ListMenu {...list_menu_props} /> }
         </div>
         <div className='right-column-group'>
 
-          { this.state.selected_model && this.renderViewInspector(this.state.selected_model) }
+          { selected_model && this.rendeRightColumnGroup()}
         </div>
       </div>
     );
@@ -162,7 +203,7 @@ export class SettingsView extends React.Component{
 
 const mapStateToProps = (state = {}, own_props)=>{
   if(state.timur.views === undefined) return {};
-  return {...state, views: state.timur.views};
+  return {views: state.timur.views};
 };
 
 const mapDispatchToProps = (dispatch, own_props)=>{
