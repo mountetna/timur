c41babc6533f9cfbcd4323ae604f13ecf2bd8343
Jason Cater <jason.cater@ucsf.edu>
2018-03-15 11:38:49 -0700

    Fixing the demographic data rendering and cleaning syntax.

--- app/assets/javascripts/components/browser/demographic_widget.jsx
+++ app/assets/javascripts/components/browser/demographic_widget.jsx
@@ -1,159 +1,187 @@
-import { connect } from 'react-redux';
-import React from 'react';
+// Framework libraries.
+import * as React from 'react';
+import * as ReactRedux from 'react-redux';
+
 import Magma from '../../magma';
 import DemographicInput from './demographic_input';
 
-class DemographicWidget extends React.Component {
+export class DemographicWidget extends React.Component{
 
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
       values: [],
       options: null
-     };
-    this.handleSubmit = this.handleSubmit.bind(this);
+    };
   }
 
-  componentWillReceiveProps() {
-    this.processDemographicData();
-  }
+  componentWillReceiveProps(next_props){
 
-  processDemographicData() {
-    let { documents, options} = this.props;
+    let {documents, options} = next_props;
+    if(Object.keys(documents).length <= 0) return;
+
+    // Interleave the documents with the dictionary/options.
     let values = [];
-    for (let obj in documents) {
-        values.push({
-          selectValue: documents[obj].name.trim(), 
-          inputValue:documents[obj].value.trim(),
-          inputType: options[documents[obj].name].type.trim()
-        });
+    for(let obj in documents){
+      values.push({
+        selectValue: documents[obj].name.trim(),
+        inputValue:documents[obj].value.trim(),
+        inputType: options[documents[obj].name].type.trim()
+      });
     }
-    this.setState({values, options});
+
+    this.setState({
+      values,
+      options
+    });
   }
 
-  optionLabels() {
+  optionLabels(){
     let items = [];
-    Object.keys(this.state.options).forEach( (key,i) => {
+    Object.keys(this.state.options).forEach((key, index)=>{
       items.push(
-        <option key={i} value={key}>{this.state.options[key].label}</option>
+        <option key={index} value={key}>{this.state.options[key].label}</option>
       ); 
     });
     return items;
   }
 
-  optionValues(option) {
+  optionValues(option){
     let items = [];
-    this.state.options[option].value.forEach( (el,i) => {
-      items.push(<option key={'option-value' + i} value={el}>{el}</option>);
+    this.state.options[option].value.forEach((el, index)=>{
+      items.push(
+        <option key={'option-value'+index} value={el}>{el}</option>
+      );
     });
     return items;
   }
 
-  createInput() {
+  createInput(){
     return(
-      this.state.values.map((el, i) => {
+      this.state.values.map((el, index)=>{
 
         let select_props = {
           className: 'demographic-select',
-          onChange: this.handleSelectChange.bind(this, i),
-          value: this.state.values[i].selectValue || ''
+          onChange: this.handleSelectChange.bind(this, index),
+          value: this.state.values[index].selectValue || ''
         };
 
         let demographics_input_props = null;
-        if (this.state.values[i].selectValue) {
+        let values = this.state.values;
+
+        if(values[index].selectValue){
           demographics_input_props = {
-            inputKey: i,
-            inputType: this.state.values[i].inputType,
-            inputValue: this.state.values[i].inputValue || '',
-            inputChange: this.handleInputChange.bind(this, i),
-            selectOptions: this.optionValues(this.state.values[i].selectValue) || '' 
+            input_key: index,
+            input_type: values[index].inputType,
+            input_value: values[index].inputValue || '',
+            select_options: this.optionValues(values[index].selectValue) || '',
+            inputChange: this.handleInputChange.bind(this, index)
           }
         }
 
-        return (<div className='demographic-group' key={i}>
+        let button_props = {
+          className: 'demographic-button remove',
+          onClick: this.removeClick.bind(this, index)
+        };
 
-          <select {...select_props}>
+        return(
+          <div className='demographic-group' key={index}>
 
-            <option defaultvalue=''>Make Selection</option>
-            {this.optionLabels()}
-          </select>
+            <select {...select_props}>
 
-          {
-            this.state.values[i].selectValue && 
-              <DemographicInput {...demographics_input_props}/>
-          }
+              <option defaultValue=''>
 
-          <button
-            className='demographic-button remove'
-            onClick={this.removeClick.bind(this, i)}>
-            
-            &#10006;
-          </button>
-        </div>)
+                Make Selection
+              </option>
+              {this.optionLabels()}
+            </select>
+            {
+              values[index].selectValue &&
+              <DemographicInput {...demographics_input_props} />
+            }
+            <button {...button_props}>
+
+              &#10006;
+            </button>
+          </div>
+        );
       }
     )
   )
  }
 
-  handleSelectChange(i, event) {
+  handleSelectChange(index, event){
     let values = [...this.state.values];
-    values[i].selectValue = event.target.value;
-    values[i].inputValue = '';
-    values[i].inputType = this.state.options[values[i].selectValue].type || '';
-    this.setState({ values });
- }
+    values[index].selectValue = event.target.value;
+    values[index].inputValue = '';
+    values[index].inputType = this.state.options[values[index].selectValue].type || '';
+    this.setState({values});
+  }
 
-  handleInputChange(i, event) {
+  handleInputChange(index, event){
     let values = [...this.state.values];
-    values[i].inputValue = event.target.value;
-    this.setState({ values });
+    values[index].inputValue = event.target.value;
+    this.setState({values});
   }
  
   addClick(event){
     event.preventDefault();
-    this.setState(prevState =>
-      ({ 
-        values: 
-          [...prevState.values, {selectValue: '', inputValue: '', inputType: ''}]
-      })
-    )
+    this.setState((prev_state)=>{
+      return {
+        values: [
+          ...prev_state.values,
+          {selectValue: '', inputValue: '', inputType: ''}
+        ]
+      };
+    });
   }
 
-  removeClick(i, event){
+  removeClick(index, event){
     event.preventDefault();
-    this.setState(prevState => {
-      let values = [...prevState.values];
-      values.splice(i, 1);
+    this.setState((prev_state)=>{
+      let values = [...prev_state.values];
+      values.splice(index, 1);
       return {values: values};
     });
   }
 
-  handleSubmit(event) {
-    alert('Testing: ' + this.state.values.join(', '));
+  handleSubmit(event){
+    console.log(this.state.values);
     event.preventDefault();
   }
 
-  render() {
-    let { record_names } = this.props;
+  render(){
+    let {record_names} = this.props;
     if (!record_names.length) return <div>No Entries</div>;
  
+    let add_btn_props = {
+      className: 'demographic-button',
+      onClick: this.addClick.bind(this)
+    };
+
+    let save_btn_props = {
+      className: 'demographic-button',
+      onClick: this.handleSubmit.bind(this)
+    };
+
     return(
-      <form onSubmit={this.handleSubmit}>
+      <div className='demographic-group'>
         {this.createInput()}
 
-      <button
-        className='demographic-button'
-        onClick={this.addClick.bind(this)}> 
-  
-        &#10010; ADD 
-      </button>
-      <input className='demographic-button' type="submit" value="SAVE"/>
-    </form>
+        <button {...add_btn_props}>
+
+          &#10010;{' ADD'}
+        </button>
+        <button {...save_btn_props}>
+
+          {'SAVE'}
+        </button>
+      </div>
     );
   }
 }
 
-const   processCSVData = () => {
+const processCSVData = ()=>{
   let csv = "name,label,value,type,description\nsubject_id,Subject ID,,string,\ninformed_consent,Date Informed Consent Signed,,date,\nfirst_name,First Name,,string,\nlast_name,Last Name,,string,\nmrn,Medical Record Number,,number,\ndate_of_birth,Date Of Birth,,date,\nage,Age (Years),,number,\nsex,Sex,Male,regex,\nsex,Sex,Female,regex,\nethnicity,Ethnicity,Hispanic Or Latino,regex,\nethnicity,Ethnicity,Not Hispanic Or Latino,regex,\nethnicity,Ethnicity,Unknown,regex,\nrace,Race,American Indian Or Alaskan Native,regex,\nrace,Race,Asian,regex,\nrace,Race,Black Or African American,regex,\nrace,Race,Native Hawaiian Or Other Pacific Islander,regex,\nrace,Race,White,regex,\nrace,Race,Unknown,regex,\nrace,Race,Other,regex,\nrace_other,Specify Other,,string,\nheight,Height (cm),,number,\nweight,Weight (kg),,number,\n";
   let lines=csv.trim().split("\n");
   let headers=lines[0].split(",");
@@ -180,10 +208,29 @@ const   processCSVData = () => {
 const mapStateToProps = (state, props)=>{
   let magma = new Magma(state);
   let template = magma.template(props.model_name);
-  let documents = magma.documents(props.model_name, props.record_names, props.filter);
+
+  let documents = magma.documents(
+    props.model_name,
+    props.record_names,
+    props.filter
+  );
+
   let options = processCSVData();
   let record_names = Object.keys(documents).sort();
-  return { options, template, documents, record_names };
+
+  return {
+    options,
+    template,
+    documents,
+    record_names
+  };
 };
 
-export default connect( mapStateToProps,{ })(DemographicWidget);
+const mapDispatchToProps = (dispatch, own_props)=>{
+  return {};
+};
+
+export const DemographicWidgetContainer = ReactRedux.connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DemographicWidget);
