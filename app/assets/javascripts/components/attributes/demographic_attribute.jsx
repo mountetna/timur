71ff4ff282188075ccfb96cb5d34ff0cd6dfdab3
Jason Cater <jason.cater@ucsf.edu>
2018-03-15 10:28:24 -0700

    General syntax clean up.

--- app/assets/javascripts/components/attributes/demographic_attribute.jsx
+++ app/assets/javascripts/components/attributes/demographic_attribute.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from 'react';
 import DemographicWidget from '../browser/demographic_widget';
 
-export default class DemographicAttribute extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { }
+export default class DemographicAttribute extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
   }
 
-  render() {
-    if (this.props.mode != 'browse') return <div className="value"></div>
+  render(){
+    if(this.props.mode != 'browse') return <div className='value'></div>;
+
+    let demo_props = {
+      model_name: this.props.attribute.model_name,
+      record_names: this.props.value
+    };
 
     return(
-      <div className="value">
-        <DemographicWidget 
-          model_name={this.props.attribute.model_name}
-          record_names={this.props.value}
-        />
-    </div>
-    )}
+      <div className='value'>
+
+        <DemographicWidget {...demo_props} />
+      </div>
+    );
+  }
 }
