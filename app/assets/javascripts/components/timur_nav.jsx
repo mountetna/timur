5cca986f564fa50927fc3dc60c88dcb2b43ede40
Jason Cater <jasondcater@gmail.com>
2018-05-08 12:57:12 -0700

    Cleaning up the navigation to work with the home page.

--- app/assets/javascripts/components/timur_nav.jsx
+++ app/assets/javascripts/components/timur_nav.jsx
@@ -2,13 +2,47 @@
 import * as React from 'react';
 import * as ReactRedux from 'react-redux';
 
-import * as TimurActions from '../actions/timur_actions';
-
 import {IdentifierSearchContainer as IdentifierSearch} from './identifier_search';
 import {HelpContainer as Help} from './help';
+import * as TimurActions from '../actions/timur_actions';
 
 export class TimurNav extends React.Component{
-  renderHalo() {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  renderTabs(){
+    let tabs = {
+      browse: Routes.browse_path(TIMUR_CONFIG.project_name),
+      search: Routes.search_path(TIMUR_CONFIG.project_name),
+      map: Routes.map_path(TIMUR_CONFIG.project_name),
+      manifests: Routes.manifests_path(TIMUR_CONFIG.project_name),
+      plots: Routes.plots_path(TIMUR_CONFIG.project_name)
+    };
+
+    return(
+      Object.keys(tabs).map((name)=>{
+
+        let tab_props = {
+          key: name,
+          className: 'nav_tab'+ (this.props.mode == name ? ' selected' : '')
+        };
+
+        return(
+          <div {...tab_props}>
+
+            <a href={tabs[name]}>
+
+              {name}
+            </a>
+          </div>
+        );
+     })
+    );
+  }
+
+  renderHalo(){
     return <div className='halo'>
       <svg>
         <circle r='25px' cx='35' cy='35'/>
@@ -27,77 +61,50 @@ export class TimurNav extends React.Component{
       </svg>
     </div>
   }
-
-  render() {
-    let tabs = {
-      browse: Routes.browse_path(TIMUR_CONFIG.project_name),
-      search: Routes.search_path(TIMUR_CONFIG.project_name),
-      map: Routes.map_path(TIMUR_CONFIG.project_name),
-      manifests: Routes.manifests_path(TIMUR_CONFIG.project_name),
-      plots: Routes.plots_path(TIMUR_CONFIG.project_name)
-    };
+  
+  render(){
 
     let login = this.props.user;
-    var heading;
-    var logo_id;
+    let heading = <span>{'Timur'}<b>{' : '}</b>{'Data Browser'}</span>;
+    let logo_id = 'normal';
 
-    if (this.props.environment == 'development') {
-      heading = <span>Timur Development</span>;
+    if(this.props.environment == 'development'){
+      heading = <span>{'Timur Development'}</span>;
       logo_id = 'dev';
     }
-    else {
-      heading = <span>Timur <b>:</b> Data Browser</span>;
-      logo_id = 'normal';
-    }
 
-    return <div id='header'>
-             <div id='logo'>
-               <a href='/'>
-                 <div id={ logo_id }
-                   className={ Object.keys(this.props.exchanges).length > 0 ? 'throb' : null }
-                 >
-                   <div className='image'/>
-                   {
-                     this.renderHalo()
-                   }
-                 </div>
-               </a>
-             </div>
-             <div id='help_float'>
-                 <Help info='timur'/>
-              </div>
-             <div id='heading'>
-               { heading }
-             </div>
-             <div id='nav'>
-               {
-                 Object.keys(tabs).map((name) =>
-                   <div key={ name } 
-                     className={ 'nav_tab' + (this.props.mode == name ? ' selected' : '') }>
-                       <a href={ tabs[name] }> { name } </a>
-                     </div>
-                 )
-               }
-               {
-                 this.props.can_edit ?
-                 <div className='nav_tab'>
-                   <a href={ Routes.activity_path(TIMUR_CONFIG.project_name) }>Activity</a>
-                 </div>
-                 : null
-               }
-               <div className='nav_tab'>
-                 <a onClick={ (e) => this.props.toggleConfig('help_shown') }>
-                 {
-                   this.props.helpShown ? 'Hide Help' : 'Help'
-                 }
-                 </a>
-               </div>
-               <IdentifierSearch/>
-               <div id='login'>
-                 { login }
-               </div>
-             </div>
-           </div>
+    return(
+      <div id='header'>
+
+        <div id='logo'>
+
+          <a href='/'>
+
+            <div id={logo_id} className={Object.keys(this.props.exchanges).length > 0 ? 'throb' : null}>
+
+              <div className='image' />
+              {this.renderHalo()}
+            </div>
+          </a>
+        </div>
+        <div id='help_float'>
+
+          <Help info='timur' />
+        </div>
+        <div id='heading'>
+
+          {heading}
+        </div>
+        <div id='nav'>
+
+          {this.props.mode !== 'home' && this.renderTabs()}
+          <div id='login'>
+            {login}
+          </div>
+          {this.props.mode !== 'home' && <IdentifierSearch />}
+        </div>
+      </div>
+    );
   }
 }
 
