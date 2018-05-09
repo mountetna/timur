1d9bc8cfee96ba2e1adef043fdd3cf4aa82e0700
Jason Cater <jason.cater@ucsf.edu>
2018-05-09 14:49:49 -0700

    Fixing the models to work with Ruby Sequel.

--- lib/models/view_pane.rb
+++ lib/models/view_pane.rb
@@ -4,11 +4,40 @@ class ViewPane < Sequel::Model
 
   def to_hash
     {
-      id: id,
       name: name,
       title: title,
       index_order: index_order,
-      attributes: Hash[ view_attributes.map{|a| [ a.name, a.to_hash ] } ]
+      attributes: Hash[ view_attributes.map{|a| [a.name, a.to_hash] }]
     }
+  end
+
+  def self.update(view_tab_id, pane_name, pane_data)
+
+    return if view_tab_id.nil? || pane_name.nil?
+
+    find_query = {
+      view_tab_id: view_tab_id.to_i,
+      name: pane_name.to_s
+    }
+
+    update_query = {
+      title: pane_data[:title].to_s,
+      index_order: pane_data[:index_order].to_i
+    }
+    update_query = find_query.merge(update_query)
+
+    pane = self.first(find_query)
+    if pane.nil?
+      pane = self.create(update_query)
+    else
+      pane = pane.set(update_query).save
+    end
+
+    # Now loop over the attributes and save if needed.
+    if pane_data.key?(:attributes) && pane_data[:attributes]
+      pane_data[:attributes].each do |attribute_name, attribute_data|
+        ViewAttribute.update(pane.id, attribute_name, attribute_data)
+      end
+    end
   end
 end
--- lib/models/view_tab.rb
+++ lib/models/view_tab.rb
@@ -1,74 +1,72 @@
 class ViewTab < Sequel::Model
   one_to_many :view_panes, order: :index_order
 
-  def self.retrieve_view(project_name, model_name)
-    # Pull all the tabs.
-    tabs = self.where(project: project_name, model: model_name)
-      .order(:index_order)
-      .all
-
-    # Return an empty view data object if there are no entries.
-    return generate_default_tab(project_name, model_name) if tabs.empty?
-
-    # Return the hashed data object.
-    return {
-      views: {
-        model_name => {
-          model_name: model_name,
-          project_name: project_name,
-          tabs: Hash[
-            tabs.map do |tab|
-              [
-                tab.name,
-                tab.to_hash
-              ]
-            end
-          ]
-        }
-      }
-    }
-  end
-
   def to_hash
     {
-      id: id,
       name: name,
       title: title,
       description: description,
       index_order: index_order,
-      panes: Hash[view_panes.map { |p| [ p.name, p.to_hash ] }]
+      panes: Hash[view_panes.map {|p| [p.name, p.to_hash] }]
     }
   end
 
   def self.generate_default_tab(project_name, model_name)
     return {
-      views: Hash[
-        model_name,
-        {
-          project_name: project_name,
-          model_name: model_name,
-          tabs: {
+      project_name: project_name,
+      model_name: model_name,
+      tabs: {
+        default: {
+          name: 'default',
+          title: '',
+          description: '',
+          index_order: 0,
+          panes: {
             default: {
-              id: nil,
               name: 'default',
               title: '',
               description: '',
               index_order: 0,
-              panes: {
-                default: {
-                  id: nil,
-                  name: 'default',
-                  title: '',
-                  description: '',
-                  index_order: 0,
-                  attributes: { # Here we should return the default attributes for the model.
-                  }
-                }
+              attributes: {
+                # Here we should return the default attributes for the model.
               }
             }
           }
         }
-      ]
+      }
     }
+  end
+
+  def self.update(project_name, model_name, tab_name, tab_data)
+
+    return if project_name.nil? || model_name.nil? || tab_name.nil?
+
+    find_query = {
+      project: project_name.to_s,
+      model: model_name.to_s,
+      name: tab_name.to_s
+    }
+
+    update_query = {
+      title: tab_data[:title].to_s,
+      description: tab_data[:description].to_s,
+      index_order: tab_data[:index_order].to_i
+    }
+    update_query = find_query.merge(update_query)
+
+    tab = self.first(find_query)
+
+    if tab.nil?
+      tab = self.create(update_query)
+    else
+      tab = tab.set(update_query).save
+    end
+
+    # Now loop over the panes and save if needed.
+    if tab_data.key?(:panes) && tab_data[:panes]
+      tab_data[:panes].each do |pane_name, pane_data|
+        ViewPane.update(tab.id, pane_name, pane_data)
+      end
+    end
   end
 end
