0bf49f6fe2705cead420be4fa3bff61b1626fcd6
Jason Cater <jason.cater@ucsf.edu>
2018-05-11 13:08:05 -0700

    Updating the selectors to more cleanly extract and form dictionary/model data.

--- lib/client/jsx/components/attributes/clinical_attribute.jsx
+++ lib/client/jsx/components/attributes/clinical_attribute.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import * as ReactRedux from 'react-redux';
 
 // Class imports.
-import Magma from '../../magma';
+import * as MagmaSelector from '../../selectors/magma_selector';
 import * as DictionarySelector from '../../selectors/dictionary_selector';
 
 // Module imports.
@@ -45,27 +45,43 @@ export class ClinicalAttribute extends React.Component{
 }
 
 const mapStateToProps = (state, own_props)=>{
-  let model_name = `${TIMUR_CONFIG.project_name}_${own_props.attribute.model_name}`;
-  let magma = new Magma(state);
-  let template = magma.template(model_name);
-  let documents = magma.documents(
-    model_name,
-    own_props.value,
-    own_props.filter
-  );
+  let project_name = TIMUR_CONFIG.project_name;
+  let model_name = `${project_name}_${own_props.attribute.model_name}`;
 
-  let dictionary = DictionarySelector.selectClinicalDictionary(
+/*
+  let dictionary = DictionarySelector.selectDictionaryByNames(
     state,
     model_name
   );
 
-  let record_names = Object.keys(documents).sort();
+  let template = MagmaSelector.selectModelTemplate(
+    state,
+    model_name
+  );
+
+  let documents = MagmaSelector.selectModelDocuments(
+    state,
+    model_name
+  );
+
+  let nested_documents = MagmaSelector.selectNestedDocuments(
+    state,
+    model_name
+  );
+
+  let nested_dictionary = DictionarySelector.selectNestedDictionary(
+    state,
+    model_name
+  );
+*/
+
+  let documents = MagmaSelector.selectModelDocuments(state, model_name);
 
   return {
-    template,
     documents,
-    record_names,
-    dictionary
+    template: MagmaSelector.selectModelTemplate(state, model_name),
+    record_names: Object.keys(documents).sort(),
+    dictionary: DictionarySelector.selectDictionaryByNames(state, model_name)
   };
 };
 
