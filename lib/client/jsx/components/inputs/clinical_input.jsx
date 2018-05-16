1f79d76ef68664057388f687cb05585f4fff7673
Jason Cater <jason.cater@ucsf.edu>
2018-05-16 14:52:43 -0700

    Adding the new base modules.

---
+++ lib/client/jsx/components/inputs/clinical_input.jsx
@@ -0,0 +1,74 @@
+// Framework libraries.
+import * as React from 'react';
+
+// Class imports.
+import DropdownSearch from '../general/dropdown_search';
+
+export default class ClinicalInput extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+    let {
+      input_type,
+      input_key,
+      input_value,
+      select_options,
+      selection_label,
+      input_class_name,
+      search_options,
+      input_placeholder,
+      inputChange,
+    } = this.props;
+
+    let input_props = {
+      className: 'clinical-input',
+      value: input_value,
+      onChange: inputChange
+    };
+
+    switch(input_type){
+      case 'string':
+        input_props['key'] = `string-${input_key}`;
+        input_props['type'] = 'text';
+        return <input {...input_props} />;
+      case 'number':
+        input_props['key'] = `number-${input_key}`;
+        input_props['type'] = 'number';
+        return <input {...input_props} />;
+      case 'date':
+        input_props['key'] = `date-${input_key}`;
+        input_props['type'] = 'datetime-local';
+
+        let timestamp = Date.parse(input_value);
+        if(!isNaN(timestamp)){
+          let dt = new Date(timestamp).toISOString().replace('Z', '');
+          input_props['value'] = dt;
+        }
+
+        return <input {...input_props} />;
+      case 'boolean':
+      case 'regex':
+      case 'select':
+        input_props['key'] = `regex-${input_key}`;
+        input_props['className'] = 'clinical-select';
+        return(
+          <select {...input_props}>
+
+            <option defaultValue=''>
+            
+              {`Make ${selection_label || ''} Selection`}
+            </option>
+            {select_options}
+          </select>
+        );
+      case 'search':
+        return <DropdownSearch {...this.props} />;
+      default:
+        console.log('Input Type Error: '+ input_type);
+        return null;
+    }
+  }
+}
