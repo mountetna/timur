076e287ed370ed6344345d94fd7cf930f0b853ed
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2018-06-11 03:02:23 +0000

    timur_lang syntax highlighting

--- lib/client/jsx/components/manifest/manifest_script.jsx
+++ lib/client/jsx/components/manifest/manifest_script.jsx
@@ -2,7 +2,22 @@
 import * as React from 'react';
 import Prism from 'prismjs';
 
+const timur_lang = {
+  comment: /\#.*$/m,
+  macro: /@[\w]+(?=\()/i,
+  variable: /@[\w]+/,
+  string: {
+          pattern: /("|')(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
+          greedy: true
+  },
+  label: /[\w]+(?=\s*:)/i,
+  template: /[{}]/,
+  function: /[\w]+(?=\()/i,
+  vector: /[\[\]]/,
+  column: /\$[\w]+/
+};
+
 export const manifestScript = (code, props)=>{
-  let __html = Prism.highlight(code, Prism.languages.javascript);
+  let __html = Prism.highlight(code, timur_lang);
   return <pre className='manifest-script' dangerouslySetInnerHTML={{__html}} />;
 };
