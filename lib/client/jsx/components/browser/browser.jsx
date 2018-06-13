c54878ab04680c3d8b2366e9ae33953f3b59057d
Jason Cater <jason.cater@ucsf.edu>
2018-06-13 12:49:46 -0700

    Using the Magma selector instead of the Mamga class. General syntax clean up.

--- lib/client/jsx/components/browser/browser.jsx
+++ lib/client/jsx/components/browser/browser.jsx
@@ -25,7 +25,18 @@ import * as ManifestActions from '../../actions/manifest_actions';
 import * as PlotActions from '../../actions/plot_actions';
 import * as TimurActions from '../../actions/timur_actions';
 import * as MagmaActions from '../../actions/magma_actions';
-import * as TabSelector from '../../selectors/tab_selector';
+
+import {
+  getAttributes,
+  getTabByIndexOrder,
+  interleaveAttributes
+} from '../../selectors/tab_selector';
+
+import {
+  selectModelDocument,
+  selectModelTemplate,
+  selectModelRevision
+} from '../../selectors/magma_selector';
 
 export class Browser extends React.Component{
 
@@ -114,7 +125,7 @@ export class Browser extends React.Component{
          * If the attributes from the tab are already present in the model's
          * document then we break.
          */
-        let tab_attr = TabSelector.getAttributes(view.tabs[tab_name]);
+        let tab_attr = getAttributes(view.tabs[tab_name]);
         for(let attr_name in doc){
           if(tab_attr.includes(attr_name)) break tab_check_loop;
         }
@@ -167,13 +178,13 @@ export class Browser extends React.Component{
     };
 
     // Select the current tab data from by the 'current_tab_index'.
-    let tab = TabSelector.getTabByIndexOrder(view.tabs, current_tab_index);
+    let tab = getTabByIndexOrder(view.tabs, current_tab_index);
 
     /*
      * Add the attribute details from the Magma model into the Timur view model.
      * and append the actual data to it.
      */
-    tab = TabSelector.interleaveAttributes(tab, template);
+    tab = interleaveAttributes(tab, template);
 
     var browser_tab_props = {
       template,
@@ -210,15 +221,13 @@ export class Browser extends React.Component{
 }
 
 const mapStateToProps = (state = {}, own_props)=>{
-
   let {model_name, record_name} = own_props;
   let mdl_nm = `${TIMUR_CONFIG.project_name}_${model_name}`;
 
-  let magma = new Magma(state);
-  let template = magma.template(mdl_nm);
-  let doc = magma.document(mdl_nm, record_name);
-  let revision = magma.revision(mdl_nm, record_name) || {};
-  let view = (state.timur.views ? state.timur.views[mdl_nm] : null);
+  let template = selectModelTemplate(state, mdl_nm);
+  let doc = selectModelDocument(state, mdl_nm, record_name);
+  let revision = selectModelRevision(state, mdl_nm, record_name);
+  let view = (state.timur.views ? state.timur.views[model_name] : null);
 
   return {
     template,
