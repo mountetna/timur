f60f57c0aad70ff5963dad86461882ca4b0b6a23
Eleanor Mark <eleanormark@gmail.com>
2018-06-13 17:00:25 -0700

    Add ae and  prior-ae data.

--- lib/client/jsx/components/attributes/plot_attributes/timeline_attribute.jsx
+++ lib/client/jsx/components/attributes/plot_attributes/timeline_attribute.jsx
@@ -5,8 +5,8 @@ import * as ReactRedux from 'react-redux';
 // Class imports.
 import {GenericPlotAttribute} from './generic_plot_attribute';
 import Consignment from '../../../models/consignment';
-import TimelinePlot from '../../plotter/plots/timeline_plot';
-import Resize from '../../plotter/resize';
+import TimelinePlot from '../../plotter/plots/timeline_plot/timeline_plot';
+import Resize from '../../plotter/plots/timeline_plot/resize';
 
 // Module imports.
 import * as ManifestActions from '../../../actions/manifest_actions';
@@ -19,9 +19,7 @@ export class TimelineAttribute extends GenericPlotAttribute{
     super(props)
 
     this.state = {
-      diagnostic: null,
-      prior_treatment: null,
-      treatment: null
+      records: null,
     };
   }
 
@@ -33,134 +31,111 @@ export class TimelineAttribute extends GenericPlotAttribute{
     ) {return null;}
 
     return {
-      diagnostic: next_props.selected_consignment.diagnostic_data,
-      prior_treatment: next_props.selected_consignment.prior_treatment_data,
-      treatment: next_props.selected_consignment.treatment_data
+      records: next_props.records
     };
   }
-  /* 
-   * This is a temporary shim. Key for date should be normalized in the
-   * database.
-   */
-  normalizeDateName(name) {
-    switch(name) {
-      case 'prior_treatment_start':
-      case 'study_treatment_start':
-        return 'start';
-      case 'prior_treatment_end':
-      case 'study_treatment_end':
-        return 'end';
-      default:
-        return name;
-    } 
-  }
 
-  uniqueLabelByDate(array){
-    array.sort((a,b) => {
-      return new Date(a.start) - new Date(b.start);
-    });
-    array.map((obj, index) => {
-      obj.label = `${obj.label} ${index+1}`;
-    });
-    return array;
-  }
-
-  normalizeD3Records(records) {
-    let d3Records = [];
-    for (let record in records) {
-      let d3Record = {};
-      d3Record.data = [];
-      d3Record.name = records[record].name;
-      d3Record.label = records[record].name.replace(/[_-]/g, " "); 
-
-      if (records[record].name === 'diagnosis_date') {
-        let time_str = new Date(records[record].value).toUTCString();
-        let parts = time_str.split(' ');
-        let utc_time_str = `${parts[1]}-${parts[2]}-${parts[3]}`;
-        d3Record.start = utc_time_str;
-      }
-
-      for (let child in records[record].children) {
-        let  name = records[record].children[child].name;
-
-        if (name === 'start' || name === 'end'){
-          d3Record[name]=records[record].children[child].value;
-        } 
-        else {
-          d3Record.data.push({
-            name: records[record].children[child].name,
-            value: records[record].children[child].value,
-            children: records[record].children[child].children
-          })
-        }
-      
-      }
-      d3Records.push(d3Record);
-    }
-
-    let prior_treatment_arr = [];
-    let treatment_arr = [];
-    let diagnostic_arr = [];
-
-    d3Records.forEach(record => {
-      switch(record.name) {
-        case 'diagnosis_date':
-          diagnostic_arr.push(record)
-          break;
-        case 'prior_treatment':
-          prior_treatment_arr.push(record)
-          break;
-        case 'study_treatment':
-          treatment_arr.push(record)
-          break;
-        default:
-            break;
-      }
-    })
-
-    prior_treatment_arr = this.uniqueLabelByDate(prior_treatment_arr);
-    treatment_arr = this.uniqueLabelByDate(treatment_arr);
-    diagnostic_arr = this.uniqueLabelByDate(diagnostic_arr);
-
-    return [...prior_treatment_arr, ...treatment_arr, ...diagnostic_arr];
-  }
-
-  render(){
-    let {diagnostic, prior_treatment, treatment} = this.state;    
-    let patient_data = [diagnostic, prior_treatment, treatment];
-    let hashed_obj = {};
-    let records;
-
-    for (let category of patient_data) {
-      if (category) {
-        for(let index = 0; index < category.row_names.length; ++index){
-          let uid = category.row_names[index];
-          hashed_obj[uid] = {
-            uid: uid,
-            parent_uid: category.rows[index][0],
-            name: this.normalizeDateName(category.rows[index][1]),
-            value: category.rows[index][2]
-          };
-        }
-      }
-    }
-    hashed_obj = nestDataset(hashed_obj, 'uid', 'parent_uid');
-    records = this.normalizeD3Records(hashed_obj);
-  
+  render(){ 
     return(
       <div id='timeline_charts' className='value-timeline'>
-        <Resize render={ (width) => (
+        {this.state.records && <Resize render={(width) => (
           <TimelinePlot 
-            all_events = {records} 
+            all_events = {this.state.records} 
             parent_width={width} 
-            hover = {this.mouseEnter}
-            leave = {this.mouseExit}
           />
-        )}/>
+        )}/>}
       </div>
     )
-  
   }
+}
+
+/* 
+ * This is a temporary shim. Key for date should be normalized in the
+ * database.
+ */
+let normalizeDateName = (name) => {
+  switch(name) {
+    case 'prior_treatment_start':
+    case 'study_treatment_start':
+    case 'start_date':
+      return 'start';
+    case 'prior_treatment_end':
+    case 'study_treatment_end':
+    case 'end_date':
+      return 'end';
+    default:
+      return name;
+  } 
+}
+
+let uniqueLabelByDate = (array) => {
+  array.sort((a,b) => {
+    return new Date(a.start) - new Date(b.start);
+  });
+  array.map((obj, index) => {
+    obj.label = `${obj.label} ${index+1}`;
+  });
+  return array;
+}
+
+let normalizeD3Records = (records) => {
+  let d3_records = [];
+  for (let record in records) {
+    let d3_record = {};
+    d3_record.data = [];
+    d3_record.name = records[record].name;
+    d3_record.label = records[record].name.replace(/[_-]/g, " "); 
+
+    if (records[record].name === 'diagnosis_date') {
+      let time_str = new Date(records[record].value).toUTCString();
+      let parts = time_str.split(' ');
+      let utc_time_str = `${parts[1]}-${parts[2]}-${parts[3]}`;
+      d3_record.start = utc_time_str;
+    }
+
+    for (let child in records[record].children) {
+      let  name = records[record].children[child].name;
+
+      if (name === 'start' || name === 'end'){
+        d3_record[name]=records[record].children[child].value;
+      } 
+      else {
+        d3_record.data.push({
+          name: records[record].children[child].name,
+          value: records[record].children[child].value,
+          children: records[record].children[child].children
+        })
+      }
+    
+    }
+    d3_records.push(d3_record);
+  }
+
+  let prior_treatment_arr = [];
+  let treatment_arr = [];
+  let diagnostic_arr = [];
+
+  d3_records.forEach(record => {
+    switch(record.name) {
+      case 'diagnosis_date':
+        diagnostic_arr.push(record)
+        break;
+      case 'prior_treatment':
+        prior_treatment_arr.push(record)
+        break;
+      case 'study_treatment':
+        treatment_arr.push(record)
+        break;
+      default:
+          break;
+    }
+  })
+
+  prior_treatment_arr = uniqueLabelByDate(prior_treatment_arr);
+  treatment_arr = uniqueLabelByDate(treatment_arr);
+  diagnostic_arr = uniqueLabelByDate(diagnostic_arr);
+
+  return [...prior_treatment_arr, ...treatment_arr, ...diagnostic_arr];
 }
 
 const mapStateToProps = (state = {}, own_props)=>{
@@ -168,6 +143,7 @@ const mapStateToProps = (state = {}, own_props)=>{
    * Pull the data required for this plot.
    */
   let selected_plot, selected_manifest, selected_consignment = undefined;
+  let records;
   
   // selected_plot = state.plots.plotsMap[own_props.attribute.plot_id];
   // if(selected_plot != undefined){
@@ -176,16 +152,115 @@ const mapStateToProps = (state = {}, own_props)=>{
 
   selected_manifest = state.manifests['179'];
 
+
   if(selected_manifest != undefined){
     selected_consignment = ConsignmentSelector.selectConsignment(
       state,
       selected_manifest.md5sum_data
     );
   }
+  if (selected_consignment) {
+    let {
+      diagnostic_data, 
+      treatment_data,
+      prior_treatment_data,
+      adverse_events,
+      prior_adverse_events
+      } = selected_consignment;
+    
+      prior_adverse_events.col_names.push('name');
+      prior_adverse_events.rows.map(row => {row.push('prior_adverse_events');});
+
+      adverse_events.col_names.push('name');
+      adverse_events.rows.map(row => {row.push('adverse_events');});
+  
+      let patient_data = [
+        diagnostic_data, 
+        treatment_data,
+        prior_treatment_data,
+      ];
+
+      let ae_patient_data = [
+        adverse_events,
+        prior_adverse_events
+      ];
+
+      let hashed_obj = {};
+ 
+      for (let category of patient_data) {
+        if (category) {
+          for(let index = 0; index < category.row_names.length; ++index){
+
+            let uid = category.row_names[index];
+            hashed_obj[uid] = {
+              uid: uid,
+              parent_uid: category.rows[index][0],
+              name: normalizeDateName(category.rows[index][1]),
+              value: category.rows[index][2]
+            };
+          }
+        }
+      }
+
+      hashed_obj = nestDataset(hashed_obj, 'uid', 'parent_uid');
+      records = normalizeD3Records(hashed_obj);
+
+      let adverse_events_arr = [];
+      let prior_adverse_events_arr = [];
+
+      for (let category of ae_patient_data){
+        if (category) {
+          for(let index = 0; index < category.rows.length; ++index){
+            let meddra_code = category.rows[index][0];
+            let start;
+            let utc_start_str;
+            let end;
+            let utc_end_str; 
+
+            if (category.rows[index][2]){
+              start = category.rows[index][2].toString().split(' ');
+              utc_start_str  = `${start[2]}-${start[1]}-${start[3]}`;
+            }
+            else {
+              utc_start_str = null
+            }
+
+            if (category.rows[index][3]){
+              end = category.rows[index][3].toString().split(' ');
+              utc_end_str  = `${end[2]}-${end[1]}-${end[3]}`;
+            }
+            else {
+              utc_end_str = null
+            }
+
+            hashed_obj[meddra_code] = {
+              grade: category.rows[index][1],
+              start: utc_start_str,
+              end: utc_end_str,
+              name: category.rows[index][4],
+              label: category.rows[index][4]
+            };
+
+            if(hashed_obj[meddra_code].name === 'adverse_events'){
+              adverse_events_arr.push(hashed_obj[meddra_code]);
+            }
+
+            if(hashed_obj[meddra_code].name === 'prior_adverse_events'){
+              prior_adverse_events_arr.push(hashed_obj[meddra_code]);
+            }
+
+          }
+        }
+      }
+      adverse_events_arr = uniqueLabelByDate(adverse_events_arr);
+      prior_adverse_events_arr = uniqueLabelByDate(prior_adverse_events_arr);
+      records = [...adverse_events_arr, ...prior_adverse_events_arr, ...records];  
+  }
   return {
     selected_plot,
     selected_manifest,
-    selected_consignment
+    selected_consignment,
+    records,
   };
 };
 
