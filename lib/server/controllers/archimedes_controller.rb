6b3a7e7d5fa56a86a3394110bc75f4f490b98999
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2018-06-19 18:18:32 +0000

    add back record_name passing into manifest

--- lib/server/controllers/archimedes_controller.rb
+++ lib/server/controllers/archimedes_controller.rb
@@ -29,7 +29,7 @@ class ArchimedesController <  Timur::Controller
         Archimedes::Manifest.new(
           token,
           @params[:project_name],
-          script
+          with_record_name(script)
         ).payload
       ]
     end.to_h
@@ -37,6 +37,10 @@ class ArchimedesController <  Timur::Controller
     success_json(consignments)
   rescue Archimedes::LanguageError => e
     raise Etna::BadRequest, e.message.to_s
+  end
+
+  def with_record_name(script)
+    @params[:record_name] ? "@record_name = '#{@params[:record_name].gsub(/'/, "''")}'\n#{script}" : script
   end
 end
 
