da0ad7a153324d23e8b47ecbe75621df1534b5f4
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2018-08-10 03:06:49 +0000

    add back rtemis client model

--- lib/models/archimedes.rb
+++ lib/models/archimedes.rb
@@ -7,6 +7,7 @@ require_relative 'archimedes/language_error'
 
 require_relative 'archimedes/matrix'
 require_relative 'archimedes/vector'
+require_relative 'rtemis'
 
 module Archimedes;
 end
---
+++ lib/models/rtemis.rb
@@ -0,0 +1,46 @@
+require 'singleton'
+
+class Rtemis
+  include Singleton
+
+  def call( func, *args )
+    response = request({
+      func: func,
+      args: args.map do |arg|
+        arg.respond_to?(:payload) ? arg.payload : arg
+      end
+    })
+    response = JSON.parse(response.body)
+    output = response["output"]
+    return parse_output(output)
+  end
+
+  private
+
+  def parse_output(output)
+    if output.is_a?(Hash)
+      if output["matrix"]
+        return Archimedes::Matrix.from_matrix(output["matrix"])
+      elsif output["vector"]
+        return Archimedes::Vector.new(output["vector"].map{|item|
+          [ item["label"], parse_output(item["value"]) ]
+        })
+      end
+    else
+      return output
+    end
+  end
+
+  private
+
+  def request data
+    uri = URI.parse(Timur.instance.config(:rtemis)[:host])
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.scheme == "https"
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.body = data.to_json
+    request["Content-Type"] = "application/json"
+    request["Accept"] = "application/json"
+    http.request(request)
+  end
+end
