56f758cb4d7d3023324a5f32ae3fc8c5fcb262c6
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2018-08-22 23:33:32 +0000

    add computation of various plot variables to plot manifest (including formerly server-side record_name input)

--- lib/client/jsx/components/attributes/plot_attribute.jsx
+++ lib/client/jsx/components/attributes/plot_attribute.jsx
@@ -1,43 +1,52 @@
 // Framework libraries.
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { selectPlot, plotScript, plotData } from '../../selectors/plot_selector';
+import { MD5, selectConsignment } from '../../selectors/consignment_selector';
 import { requestPlot } from '../../actions/plot_actions';
 import { requestConsignments } from '../../actions/manifest_actions';
+import BarGraph from '../plotter_d3_v5/plots/bar_plot/bar_graph';
+import BarPlot from '../plotter/plots/bar_plot';
+import BoxGraph from '../plotter_d3_v5/plots/box_plot/box_graph';
+import Histogram from '../plotter/plots/histogram';
+import LineGraph, { configureLineGraph } from '../plotter_d3_v5/plots/line_plot/line_graph';
+import StackedBarPlot from '../plotter/plots/stacked_bar_plot';
+import Swarm from '../plotter/plots/swarm';
+import Resize from '../plotter_d3_v5/resize';
+import TimelinePlot from '../plotter_d3_v5/plots/timeline_plot/timeline_graph';
 
-// The basic plot attribute - it renders ALL plots using plot.plot_type
 class PlotAttribute extends React.Component {
   componentDidMount() {
     let {
-      document,
-      template,
-      attribute,
-      plot,
-      consignment
+      attribute, plot, data,
+      document: { name: record_name },
+      requestPlot, requestConsignments
     } = this.props;
 
-    if (!plot && !consignment)
+    if (!plot && !data)
       requestPlot(
         attribute.plot_id,
-        (plot)=> requestConsignments([plot.script])
-      )
-    else if (plot && !consignment)
-      requestConsignments([plot.script])
+        (plot) => requestConsignments([plotScript(plot, { record_name })])
+      );
+    else if (plot && !data)
+      requestConsignments([plotScript(plot, { record_name })]);
   }
 
   render() {
-    let { plot, consignment } = this.props;
+    let { plot, data } = this.props;
 
-    if (!plot) return null;
+    if (!plot || !data) return null;
 
-    let { plot_type, configuration } = plot;
-
-    let PlotComponent;
+    let { plot_type, configuration: { layout } } = plot;
+    let PlotComponent, configure;
     switch(plot_type) {
       case "boxplot":
-        PlotComponent = BoxPlot; break;
+        return null;
+        PlotComponent = BoxGraph; break;
       case "barplot":
         PlotComponent = BarPlot; break;
       case "bargraph":
+        return null;
         PlotComponent = BarGraph; break;
       case "stackedbar":
         PlotComponent = StackedBarPlot; break;
@@ -56,9 +65,8 @@ class PlotAttribute extends React.Component {
     return(
       <div className='value'>
         <Resize render={width => (
-          <PlotComponent
-            configuration={configuration}
-            consignment={consignment}
+          <PlotComponent data={data}
+            layout={ layout }
           parent_width={width} />
         )}/>
       </div>
@@ -68,10 +76,11 @@ class PlotAttribute extends React.Component {
 
 export default connect(
   // map state
-  (state, {attribute: { plot_id }}) => {
-    let plot = selectPlot(state, plot_id);
-    let consignment = plot ? selectConsignment(state, MD5(plot.script)) : null;
-    return { plot, consignment };
+  (state, {document: { name: record_name }, attribute: { plot_id }}) => {
+    let plot = selectPlot(state, plot_id, { record_name });
+    let consignment = plot ? selectConsignment(state, MD5(plot.plotScript)) : null;
+    let data = consignment ? plotData(plot, consignment) : null;
+    return { plot, data };
   },
   // map dispatch
   { requestPlot, requestConsignments }
