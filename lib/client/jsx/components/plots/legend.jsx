ee9535ea0d82f5850a1053217d4fd3eafe26c521
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2019-03-08 18:30:56 +0000

    simplify legend component

--- lib/client/jsx/components/plots/legend.jsx
+++ lib/client/jsx/components/plots/legend.jsx
@@ -1,35 +1,15 @@
 import React, {Component} from 'react';
 
-export default class Legend extends Component{
-  render(){
-    let {labels} = this.props;
-    let categories = [];
-
-    for(let item in labels){
-
-      let legend_category_props = {
-        key: labels[item].color,
-        className: 'category-group',
-      };
-
-      let color_props = {
-        className: 'label-rect',
-        style: {background: labels[item].color}
-      };
-
-      categories.push(
-        <div {...legend_category_props}>
-          <div {...color_props}></div>
-          <div className='label-text'>{labels[item].text}</div>
+const Legend = ({labels}) =>
+  <div className='legend-container'>
+    {
+      Object.keys(labels).map(label =>
+        <div key={ label } className='category-group'>
+          <div className='label-rect' style={{background: labels[label].color}}/>
+          <div className='label-text'>{labels[label].text}</div>
         </div>
-      );
+      )
     }
+  </div>;
 
-    return(
-        <div className='legend-container'>
-          {categories}
-        </div>
-    );
-  }
-}
-
+export default Legend;
