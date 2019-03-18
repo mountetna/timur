611f1b2950f61f696d0bf91e28ddcb8a8d61b71e
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2019-03-18 22:19:48 +0000

    include non-expression variables in plot data, expand to show non-required vars in editor

--- lib/client/jsx/components/plotter_d3_v5/plot_series.jsx
+++ lib/client/jsx/components/plotter_d3_v5/plot_series.jsx
@@ -1,27 +1,59 @@
 import * as React from 'react';
 import TextInput from '../inputs/text_input';
 
+const SeriesInput = ({name,config,value,onChange}) =>
+  <TextInput header={name} value={value} onChange={onChange}/>;
+
 class PlotSeries extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show_optional: null }
+  }
+
   render(){
+    const { show_optional } = this.state;
     const {plot_series, onChange, series_config, onDelete} = this.props;
     const {variables, series_type, name} = plot_series;
+
+    const required_variables = Object.keys(series_config.variables).filter(
+      var_name => series_config.variables[var_name].required
+    );
+    const optional_variables = Object.keys(series_config.variables).filter(
+      var_name => !series_config.variables[var_name].required
+    );
+
 
     return(
       <div className='ps-card-container'>
         <div className='ps-header'>
           {series_type}
-          <i onClick={onDelete} className='right fa fa-lg'>&times;</i>
+          <i onClick={onDelete} className='fas fa-times'/>
         </div>
         <TextInput header='name' value={name} onChange={
            new_value => onChange('name', new_value)
         }/>
         {
-          Object.keys(series_config.variables).map(var_name =>
-            <TextInput key={var_name}
-              header={var_name} value={variables[var_name]}
-              onChange={
-                new_value => onChange('variables', {...variables, [var_name]: new_value})
-              }
+          required_variables.map(var_name =>
+            <SeriesInput key={ var_name }
+              config={series_config.variables[var_name]}
+              value={variables[var_name]}
+              name={var_name}
+              onChange={ new_value => onChange('variables', {...variables, [var_name]: new_value}) }
+            />
+          )
+        }
+        { !show_optional && optional_variables.length > 0 && <div className='show' onClick={ () => this.setState({ show_optional: true }) }>
+          <i className='fas fa-plus'/>
+            More options
+          </div>
+        }
+        {
+          show_optional && optional_variables.map(var_name =>
+            <SeriesInput key={ var_name }
+              config={series_config.variables[var_name]}
+              value={variables[var_name]}
+              name={var_name}
+              onChange={ new_value => onChange('variables', {...variables, [var_name]: new_value}) }
             />
           )
         }
