b5d5064c376dd30206ba87d4934fb9c5af753f4d
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2019-03-26 16:37:57 +0000

    update template style, fix comment style error in script editor

--- lib/client/jsx/components/manifest/manifest_script.jsx
+++ lib/client/jsx/components/manifest/manifest_script.jsx
@@ -10,14 +10,23 @@ const timur_lang = [
   { regex: /@[\w]+/, token: 'variable'},
   { regex: /("|')(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/, token: 'string' },
   { regex: /[\w]+(?=\s*:)/i, token: 'label'},
-  { regex: /[{}]/, token: 'template'},
+  // single-line template
+  { regex: /\{.*\}/, token: 'template'},
+  // start of multi-line template
+  { regex: /\{/, token: 'template', next: 'template'},
   { regex: /[\w]+(?=\()/i, token: 'function'},
   { regex: /[\[\]]/, token: 'vector'},
   { regex: /\$[\w]+/, token: 'column'}
 ];
 
+const template = [
+  { regex: /\}/, token: 'template', next: 'start'},
+  { regex: /.*/, token: 'template'}
+];
+
 defineSimpleMode('timur_lang', {
   start: timur_lang,
+  template,
   meta: {
     lineComment: "#"
   }
