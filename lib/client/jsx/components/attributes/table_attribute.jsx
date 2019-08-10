a96ac2d84cae545935bed3e1823152c309224c55
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2019-08-10 18:19:45 +0000

    simplify table attribute

--- lib/client/jsx/components/attributes/table_attribute.jsx
+++ lib/client/jsx/components/attributes/table_attribute.jsx
@@ -4,28 +4,21 @@ import React from 'react'
 export default class TableAttribute extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { filter: "", current_page: 0 }
+    this.state = { current_page: 0 }
   }
 
   render() {
     let { mode, attribute, value } = this.props;
-    let { current_page, filter } = this.state;
+    let { current_page } = this.state;
 
-    if (mode != 'browse') return <div className="value"></div>;
+    if (mode != 'browse') return <div className="value"/>;
 
     if (!value || !value.length) return <div className='value'>No data</div>;
 
     return <div className="value">
-      <input placeholder='Filter query' className='filter' type='text' onChange={
-        e => this.setState({ current_page: 0, filter: e.target.value })
-      }/>
-      <input className='export' type='button' value={'\u21af TSV'} onClick={
-        () => requestTSV(model_name, record_names)
-      }/>
       <ModelViewer
         page_size={ 10 }
         pages={ -1 }
-        filter={ filter }
         model_name={ attribute.model_name }
         record_names={ value }/>
     </div>
