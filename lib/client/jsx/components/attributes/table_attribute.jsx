e053591d3381a6c529eb09ce5b8de6678a22611a
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2019-08-12 23:48:05 +0000

    restore filter to table attribute

--- lib/client/jsx/components/attributes/table_attribute.jsx
+++ lib/client/jsx/components/attributes/table_attribute.jsx
@@ -1,18 +1,30 @@
 import ModelViewer from '../model_viewer'
 import React from 'react'
 
-const TableAttribute = ({ mode, attribute, value }) => {
-  if (mode != 'browse') return <div className='attribute'/>;
+export default class TableAttribute extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { filter: "", current_page: 0 }
+  }
 
-  if (!value || !value.length) return <div className='attribute'>No data</div>;
+  render() {
+    let { mode, attribute, value } = this.props;
+    let { current_page, filter } = this.state;
 
-  return <div className='attribute'>
-    <ModelViewer
-      page_size={ 10 }
-      pages={ -1 }
-      model_name={ attribute.model_name }
-      record_names={ value }/>
-  </div>
+    if (mode != 'browse') return <div className="value"></div>;
+
+    if (!value || !value.length) return <div className='value'>No data</div>;
+
+    return <div className='attribute'>
+      <input placeholder='Filter query' className='filter' type='text' onChange={
+        e => this.setState({ current_page: 0, filter: e.target.value })
+      }/>
+      <ModelViewer
+        page_size={ 10 }
+        pages={ -1 }
+        filter={ filter }
+        model_name={ attribute.model_name }
+        record_names={ value }/>
+    </div>
+  }
 }
-
-export default TableAttribute;
