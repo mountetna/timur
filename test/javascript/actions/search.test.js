4ee4d6fdf7b44e53bf1507eada5dd64283c4a028
Cole S <4930129+coleshaw@users.noreply.github.com>
2020-07-14 18:28:33 +0000

    simple tests for existing actions and reducers

---
+++ test/javascript/actions/search.test.js
@@ -0,0 +1,76 @@
+import * as actions from '../../../lib/client/jsx/actions/search_actions';
+import {
+  mockStore,
+  mockDate,
+  mockFetch,
+  setConfig,
+  stubUrl,
+  cleanStubs
+} from '../helpers';
+
+const PROJECT_NAME = 'labors';
+
+describe('cacheSearchPage', () => {
+  it('dcaches the given page', () => {
+    const store = mockStore({});
+    const page = {};
+    const model_name = 'monsters';
+    const record_names = ['Nemean Lion', 'Lernean Hydra', 'Augean Stables'];
+    const clear_cache = false;
+
+    store.dispatch(
+      actions.cacheSearchPage(page, model_name, record_names, clear_cache)
+    );
+
+    expect(store.getActions()).toEqual([
+      {
+        page,
+        model_name,
+        record_names,
+        clear_cache,
+        type: actions.CACHE_SEARCH_PAGE
+      }
+    ]);
+  });
+});
+
+describe('emptySearchCache', () => {
+  it('clears out the search cache', () => {
+    const store = mockStore({});
+
+    store.dispatch(actions.emptySearchCache());
+
+    expect(store.getActions()).toEqual([{type: actions.EMPTY_SEARCH_CACHE}]);
+  });
+});
+
+describe('setSearchPage', () => {
+  it('sets the given page as the search page', () => {
+    const store = mockStore({});
+    const page = {
+      title: 'A test search page',
+      documents: [
+        {
+          name: 'First document?'
+        }
+      ]
+    };
+
+    store.dispatch(actions.setSearchPage(page));
+
+    expect(store.getActions()).toEqual([{page, type: actions.SET_SEARCH_PAGE}]);
+  });
+});
+
+describe('setSearchPageSize', () => {
+  it('sets the search page size to the specified length', () => {
+    const store = mockStore({});
+    const page_size = 42;
+
+    store.dispatch(actions.setSearchPageSize(page_size));
+
+    expect(store.getActions()).toEqual([
+      {page_size, type: actions.SET_SEARCH_PAGE_SIZE}
+    ]);
+  });
+});
