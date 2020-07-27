7262295612687452a8b61e89c7521050edf3186a
coleshaw <4930129+coleshaw@users.noreply.github.com>
2020-07-27 14:34:28 -0400

    Order timur view (#32)
    * Sort attribute records by natural sort, if possible

--- lib/client/jsx/components/attributes/collection_attribute.jsx
+++ lib/client/jsx/components/attributes/collection_attribute.jsx
@@ -1,40 +1,55 @@
 import MagmaLink from '../magma_link';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
-import { reviseDocument } from '../../actions/magma_actions';
-import React, { Component } from 'react';
+import {reviseDocument} from '../../actions/magma_actions';
+import React, {Component, useMemo} from 'react';
 import ListInput from '../inputs/list_input';
 import SlowTextInput from '../inputs/slow_text_input';
 
-const CollectionAttribute = ({ mode, value, revised_value,
-  document, template, attribute, reviseDocument }) => {
-  if (mode != 'edit') return (
-    <div className='attribute'>
-      <div className='collection'>
-        {
-          (value || []).map( link =>
-            <div key={ link } className='collection_item'>
-              <MagmaLink link={ link } model={ attribute.link_model_name }/>
+const CollectionAttribute = ({
+  mode,
+  value,
+  revised_value,
+  document,
+  template,
+  attribute,
+  reviseDocument
+}) => {
+  if (mode != 'edit') {
+    var collator = new Intl.Collator(undefined, {
+      numeric: true,
+      sensitivity: 'base'
+    });
+    const sortedCollection = useMemo(
+      () => [...(value || [])].sort(collator.compare),
+      [value]
+    );
+    return (
+      <div className='attribute'>
+        <div className='collection'>
+          {sortedCollection.map((link) => (
+            <div key={link} className='collection_item'>
+              <MagmaLink link={link} model={attribute.link_model_name} />
             </div>
-          )
-        }
+          ))}
+        </div>
       </div>
+    );
+  }
+
+  return (
+    <div className='attribute'>
+      <ListInput
+        placeholder='New or existing ID'
+        className='link_text'
+        values={revised_value || []}
+        itemInput={SlowTextInput}
+        onChange={(links) =>
+          reviseDocument(document, template, attribute, links)
+        }
+      />
     </div>
   );
+};
 
-  return <div className='attribute'>
-    <ListInput
-      placeholder='New or existing ID'
-      className='link_text'
-      values={ revised_value || [] }
-      itemInput={ SlowTextInput }
-      onChange={
-        links => reviseDocument( document, template, attribute, links)
-      }/>
-  </div>;
-}
-
-export default connect(
-  null,
-  { reviseDocument }
-)(CollectionAttribute);
+export default connect(null, {reviseDocument})(CollectionAttribute);
