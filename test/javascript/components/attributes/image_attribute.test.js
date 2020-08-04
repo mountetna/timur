2e4bd73e231b840e9ef585b6ba59333fce944cdf
Cole <4930129+coleshaw@users.noreply.github.com>
2020-08-04 15:54:33 -0400

    update image attribute to use thumbnail URL param

--- test/javascript/components/attributes/image_attribute.test.js
+++ test/javascript/components/attributes/image_attribute.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
-import { mockStore } from '../../helpers';
+import {mount, shallow} from 'enzyme';
+import {mockStore} from '../../helpers';
 import renderer from 'react-test-renderer';
 import ButtonBar from '../../../../lib/client/jsx/components/button_bar';
-import { STUB } from '../../../../lib/client/jsx/components/attributes/file_attribute';
+import {STUB} from '../../../../lib/client/jsx/components/attributes/file_attribute';
 import ImageAttribute from '../../../../lib/client/jsx/components/attributes/image_attribute';
 
 import * as magmaActions from '../../../../lib/client/jsx/actions/magma_actions';
@@ -22,14 +22,14 @@ describe('ImageAttribute', () => {
   it('renders the button bar while editing', () => {
     const component = mount(
       <ImageAttribute
-        model_name="conquests"
-        record_name="Persia"
+        model_name='conquests'
+        record_name='Persia'
         template={null}
         value={null}
-        mode="edit"
-        attribute="gravatar"
-        document="Timur"
-        revised_value=""
+        mode='edit'
+        attribute='gravatar'
+        document='Timur'
+        revised_value=''
         store={store}
       />
     );
@@ -40,14 +40,14 @@ describe('ImageAttribute', () => {
     const tree = renderer
       .create(
         <ImageAttribute
-          model_name="conquests"
-          record_name="Persia"
+          model_name='conquests'
+          record_name='Persia'
           template={null}
           value={null}
-          mode="edit"
-          attribute="gravatar"
-          document="Timur"
-          revised_value=""
+          mode='edit'
+          attribute='gravatar'
+          document='Timur'
+          revised_value=''
           store={store}
         />
       )
@@ -57,36 +57,82 @@ describe('ImageAttribute', () => {
   });
 
   it('does not render action buttons while not editing', () => {
-    const value = null;
+    const value = {
+      url: 'https://example.com?HMAC-header=foo'
+    };
 
     const component = mount(
       <ImageAttribute
-        model_name="conquests"
-        record_name="Persia"
+        model_name='conquests'
+        record_name='Persia'
         template={null}
         value={value}
-        mode="view"
-        attribute="gravatar"
-        document="Timur"
-        revised_value=""
+        mode='view'
+        attribute='gravatar'
+        document='Timur'
+        revised_value=''
         store={store}
       />
     );
 
-    const buttons = component.find('file-buttons');
+    const buttons = component.find('.file-buttons');
     expect(buttons.exists()).toBeFalsy();
+
+    const thumbnail = component.find('.image-thumbnail');
+    expect(thumbnail.exists()).toBeTruthy();
 
     const tree = renderer
       .create(
         <ImageAttribute
-          model_name="conquests"
-          record_name="Persia"
+          model_name='conquests'
+          record_name='Persia'
           template={null}
           value={value}
-          mode="view"
-          attribute="gravatar"
-          document="Timur"
-          revised_value=""
+          mode='view'
+          attribute='gravatar'
+          document='Timur'
+          revised_value=''
+          store={store}
+        />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders thumbnail in view mode if file exists', () => {
+    const value = {
+      url: 'https://example.com?HMAC-header=foo'
+    };
+
+    const component = mount(
+      <ImageAttribute
+        model_name='conquests'
+        record_name='Persia'
+        template={null}
+        value={value}
+        mode='view'
+        attribute='gravatar'
+        document='Timur'
+        revised_value=''
+        store={store}
+      />
+    );
+
+    const thumbnail = component.find('.image-thumbnail');
+    expect(thumbnail.exists()).toBeTruthy();
+
+    const tree = renderer
+      .create(
+        <ImageAttribute
+          model_name='conquests'
+          record_name='Persia'
+          template={null}
+          value={value}
+          mode='view'
+          attribute='gravatar'
+          document='Timur'
+          revised_value=''
           store={store}
         />
       )
@@ -96,36 +142,18 @@ describe('ImageAttribute', () => {
   });
 
   it('renders blank paths correctly while not editing', () => {
-    let value = { path: '::blank' };
+    let value = {path: '::blank'};
 
     let component = mount(
       <ImageAttribute
-        model_name="conquests"
-        record_name="Persia"
+        model_name='conquests'
+        record_name='Persia'
         template={null}
         value={value}
-        mode="view"
-        attribute="gravatar"
-        document="Timur"
-        revised_value=""
-        store={store}
-      />
-    );
-
-    expect(component.text().trim()).toEqual('Blank file');
-
-    value = '::blank';
-
-    component = mount(
-      <ImageAttribute
-        model_name="conquests"
-        record_name="Persia"
-        template={null}
-        value={value}
-        mode="view"
-        attribute="gravatar"
-        document="Timur"
-        revised_value=""
+        mode='view'
+        attribute='gravatar'
+        document='Timur'
+        revised_value=''
         store={store}
       />
     );
@@ -135,14 +163,14 @@ describe('ImageAttribute', () => {
     const tree = renderer
       .create(
         <ImageAttribute
-          model_name="conquests"
-          record_name="Persia"
+          model_name='conquests'
+          record_name='Persia'
           template={null}
           value={value}
-          mode="view"
-          attribute="gravatar"
-          document="Timur"
-          revised_value=""
+          mode='view'
+          attribute='gravatar'
+          document='Timur'
+          revised_value=''
           store={store}
         />
       )
@@ -156,14 +184,14 @@ describe('ImageAttribute', () => {
 
     const component = mount(
       <ImageAttribute
-        model_name="conquests"
-        record_name="Persia"
+        model_name='conquests'
+        record_name='Persia'
         template={null}
         value={value}
-        mode="view"
-        attribute="gravatar"
-        document="Timur"
-        revised_value=""
+        mode='view'
+        attribute='gravatar'
+        document='Timur'
+        revised_value=''
         store={store}
       />
     );
@@ -173,56 +201,14 @@ describe('ImageAttribute', () => {
     const tree = renderer
       .create(
         <ImageAttribute
-          model_name="conquests"
-          record_name="Persia"
+          model_name='conquests'
+          record_name='Persia'
           template={null}
           value={value}
-          mode="view"
-          attribute="gravatar"
-          document="Timur"
-          revised_value=""
-          store={store}
-        />
-      )
-      .toJSON();
-
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('renders existing files correctly when not editing', () => {
-    const value = new File(
-      ['Believe me, you are but pismire ant:'],
-      'conquest.txt',
-      { type: 'text/plain' }
-    );
-
-    const component = mount(
-      <ImageAttribute
-        model_name="conquests"
-        record_name="Persia"
-        template={null}
-        value={value}
-        mode="view"
-        attribute="gravatar"
-        document="Timur"
-        revised_value=""
-        store={store}
-      />
-    );
-
-    expect(component.text().trim()).toEqual('conquest.txt (text/plain)');
-
-    const tree = renderer
-      .create(
-        <ImageAttribute
-          model_name="conquests"
-          record_name="Persia"
-          template={null}
-          value={value}
-          mode="view"
-          attribute="gravatar"
-          document="Timur"
-          revised_value=""
+          mode='view'
+          attribute='gravatar'
+          document='Timur'
+          revised_value=''
           store={store}
         />
       )
@@ -234,14 +220,14 @@ describe('ImageAttribute', () => {
   it('accepts a valid Metis path', () => {
     const component = mount(
       <ImageAttribute
-        model_name="conquests"
-        record_name="Persia"
-        template={{ name: 'Conquests', identifier: 1 }}
+        model_name='conquests'
+        record_name='Persia'
+        template={{name: 'Conquests', identifier: 1}}
         value={null}
-        mode="edit"
-        attribute={{ name: 'ExpansionPlans' }}
-        document={{ '1': 'Timur' }}
-        revised_value=""
+        mode='edit'
+        attribute={{name: 'ExpansionPlans'}}
+        document={{'1': 'Timur'}}
+        revised_value=''
         store={store}
       />
     );
@@ -264,7 +250,7 @@ describe('ImageAttribute', () => {
         model_name: 'Conquests',
         record_name: 'Timur',
         revision: {
-          ['ExpansionPlans']: { path: 'metis://project/bucket/file_name.txt' }
+          ['ExpansionPlans']: {path: 'metis://project/bucket/file_name.txt'}
         }
       }
     ]);
@@ -273,14 +259,14 @@ describe('ImageAttribute', () => {
   it('shows an error message when given an invalid Metis path', () => {
     const component = mount(
       <ImageAttribute
-        model_name="conquests"
-        record_name="Persia"
-        template={{ name: 'Conquests', identifier: 1 }}
+        model_name='conquests'
+        record_name='Persia'
+        template={{name: 'Conquests', identifier: 1}}
         value={null}
-        mode="edit"
-        attribute={{ name: 'ExpansionPlans' }}
-        document={{ '1': 'Timur' }}
-        revised_value=""
+        mode='edit'
+        attribute={{name: 'ExpansionPlans'}}
+        document={{'1': 'Timur'}}
+        revised_value=''
         store={store}
       />
     );
@@ -303,14 +289,14 @@ describe('ImageAttribute', () => {
   it('dispatches an action to mark a file as blank', () => {
     const component = mount(
       <ImageAttribute
-        model_name="conquests"
-        record_name="Persia"
-        template={{ name: 'Conquests', identifier: 1 }}
+        model_name='conquests'
+        record_name='Persia'
+        template={{name: 'Conquests', identifier: 1}}
         value={null}
-        mode="edit"
-        attribute={{ name: 'ExpansionPlans' }}
-        document={{ '1': 'Timur' }}
-        revised_value=""
+        mode='edit'
+        attribute={{name: 'ExpansionPlans'}}
+        document={{'1': 'Timur'}}
+        revised_value=''
         store={store}
       />
     );
@@ -325,7 +311,7 @@ describe('ImageAttribute', () => {
         model_name: 'Conquests',
         record_name: 'Timur',
         revision: {
-          ['ExpansionPlans']: { path: STUB }
+          ['ExpansionPlans']: {path: STUB}
         }
       }
     ]);
@@ -334,14 +320,14 @@ describe('ImageAttribute', () => {
   it('dispatches an action to remove a file', () => {
     const component = mount(
       <ImageAttribute
-        model_name="conquests"
-        record_name="Persia"
-        template={{ name: 'Conquests', identifier: 1 }}
+        model_name='conquests'
+        record_name='Persia'
+        template={{name: 'Conquests', identifier: 1}}
         value={null}
-        mode="edit"
-        attribute={{ name: 'ExpansionPlans' }}
-        document={{ '1': 'Timur' }}
-        revised_value=""
+        mode='edit'
+        attribute={{name: 'ExpansionPlans'}}
+        document={{'1': 'Timur'}}
+        revised_value=''
         store={store}
       />
     );
@@ -356,7 +342,7 @@ describe('ImageAttribute', () => {
         model_name: 'Conquests',
         record_name: 'Timur',
         revision: {
-          ['ExpansionPlans']: { path: null }
+          ['ExpansionPlans']: {path: null}
         }
       }
     ]);
