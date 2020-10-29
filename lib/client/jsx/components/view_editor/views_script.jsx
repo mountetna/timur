33ea29d1e4b4566196962a6b063d405f759771ba
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2020-10-28 23:22:29 -0700

    v. 0.0.1.gvai - added codemirror, unconnected to state

--- lib/client/jsx/components/view_editor/views_script.jsx
+++ lib/client/jsx/components/view_editor/views_script.jsx
@@ -5,20 +5,11 @@ import { Controlled as CodeMirror } from 'react-codemirror2';
 
 function ViewScript(props) {
 
-  /*
-  scroll() {
-    let { is_editing } = this.props;
-    if (this.editor && this.script) {
-      if (is_editing) this.script.scrollTop = this.editor.scrollTop;
-      else this.editor.scrollTop = this.script.scrollTop;
-    }
-  }
-   */
-
   let { is_editing, onChange, script } = props;
   let className = is_editing ? 'view-script editing' : 'view-script';
   return (
       <div className={className}>
+
         <CodeMirror
             options = {{
               readOnly: is_editing ? false : 'no-cursor',
