8f0a7fb757e09473d9a91de97f58bec2243e19a8
Cole <4930129+coleshaw@users.noreply.github.com>
2020-11-03 15:09:50 -0500

    WIP: refactor file attribute to functional component

--- lib/client/jsx/components/attributes/file_attribute.jsx
+++ lib/client/jsx/components/attributes/file_attribute.jsx
@@ -1,7 +1,10 @@
 // Framework libraries.
-import * as React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {connect} from 'react-redux';
 import Modal from 'react-modal';
+
+import {useReduxState} from 'etna-js/hooks/useReduxState';
+import {useActionInvoker} from 'etna-js/hooks/useActionInvoker';
 
 import {
   reviseDocument,
@@ -41,132 +44,236 @@ const customStyles = {
 
 const FileValue = ({value}) =>
   !value ? (
-    <span className="file-missing"> No file </span>
+    <span className='file-missing'> No file </span>
   ) : value instanceof File ? (
-    <span className="file-upload">
+    <span className='file-upload'>
       {' '}
       {value.name} ({value.type}){' '}
     </span>
-  ) : value == STUB || value.path == STUB ? (
-    <span className="file-blank"> Blank file </span>
+  ) : value === STUB || value.path === STUB ? (
+    <span className='file-blank'> Blank file </span>
+  ) : value === TEMP || value.path === TEMP ? (
+    <span className='file-blank'> {value.original_files[0].name} </span>
   ) : (
     <a href={value.url}> {value.path} </a>
   );
 
-class FileAttribute extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {metis: false, error: false};
-  }
+export default function FileAttribute(props) {
+  const invoke = useActionInvoker();
+  const [metis, setMetis] = useState(false);
+  const [error, setError] = useState(false);
+  const fileInputRef = useRef(null);
 
-  render() {
-    let {
-      mode,
-      value,
-      revised_value,
-      document,
-      template,
-      attribute,
-      reviseDocument
-    } = this.props;
-    let {metis} = this.state;
+  const {
+    metisSelector,
+    closeModal,
+    selectMetisFile,
+    formatFileRevision,
+    setTempRevision
+  } = useFileActions(metis, error, setMetis, setError, props);
 
-    if (mode != 'edit')
-      return (
-        <div className="attribute file">
-          <FileValue value={value} />
-        </div>
-      );
+  let {mode, value, revised_value, document, template, attribute} = props;
 
-    let buttons = [
-      // { type: 'upload',
-      //   click: () => this.input.click(),
-      //   title: 'Upload a file from your computer' },
-      {
-        type: 'cloud',
-        click: () => this.setState({metis: true}),
-        title: 'Link a file from Metis'
-      },
-      {
-        type: 'stub',
-        click: () =>
-          reviseDocument(
-            document,
-            template,
-            attribute,
-            this.formatFileRevision(STUB)
-          ),
-        title: 'Mark this file as blank'
-      },
-      {
-        type: 'remove',
-        click: () =>
-          reviseDocument(
-            document,
-            template,
-            attribute,
-            this.formatFileRevision(null)
-          ),
-        title: 'Remove this file link'
-      }
-    ];
-
+  if (mode != 'edit')
     return (
-      <div className="attribute file">
-        <input
-          type="file"
-          style={{display: 'none'}}
-          ref={(input) => (this.input = input)}
-          onChange={(e) => this.getTempUrl(e)}
-        />
-        {metis && this.metisSelector()}
-        <ButtonBar className="file-buttons" buttons={buttons} />
-        <FileValue value={revised_value} />
+      <div className='attribute file'>
+        <FileValue value={value} />
       </div>
     );
-  }
 
-  metisSelector() {
+  let buttons = [
+    {
+      type: 'upload',
+      click: () => fileInputRef.current.click(),
+      title: 'Upload a file from your computer'
+    },
+    {
+      type: 'cloud',
+      click: () => setMetis(true),
+      title: 'Link a file from Metis'
+    },
+    {
+      type: 'stub',
+      click: () =>
+        invoke(
+          reviseDocument(
+            document,
+            template,
+            attribute,
+            formatFileRevision(STUB)
+          )
+        ),
+      title: 'Mark this file as blank'
+    },
+    {
+      type: 'remove',
+      click: () =>
+        invoke(
+          reviseDocument(
+            document,
+            template,
+            attribute,
+            formatFileRevision(null)
+          )
+        ),
+      title: 'Remove this file link'
+    }
+  ];
+
+  return (
+    <div className='attribute file'>
+      <input
+        type='file'
+        style={{display: 'none'}}
+        ref={fileInputRef}
+        onChange={setTempRevision}
+      />
+      {metis && metisSelector()}
+      <ButtonBar className='file-buttons' buttons={buttons} />
+      <FileValue value={revised_value} />
+    </div>
+  );
+
+  // const browserState = useReduxState(
+  //   browserStateOf({model_name, record_name, tab_name})
+  // );
+  // const {view, record, tab, revision, template, can_edit} = browserState;
+  // const [mode, setMode] = useState('loading');
+  // const loading = !view || !template || !record || !tab_name;
+  // const {cancelEdits, approveEdits} = useEditActions(setMode, browserState);
+  // const {selectOrShowTab, selectDefaultTab, selectTab, showTab} = useTabActions(
+  //   browserState,
+  //   setMode
+  // );
+}
+
+// class FileAttribute extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {metis: false, error: false};
+//   }
+
+//   render() {
+//     let {
+//       mode,
+//       value,
+//       revised_value,
+//       document,
+//       template,
+//       attribute,
+//       reviseDocument
+//     } = this.props;
+//     let {metis} = this.state;
+//     console.log('revised value', revised_value);
+//     if (mode != 'edit')
+//       return (
+//         <div className='attribute file'>
+//           <FileValue value={value} />
+//         </div>
+//       );
+
+//     let buttons = [
+//       {
+//         type: 'upload',
+//         click: () => this.input.click(),
+//         title: 'Upload a file from your computer'
+//       },
+//       {
+//         type: 'cloud',
+//         click: () => this.setState({metis: true}),
+//         title: 'Link a file from Metis'
+//       },
+//       {
+//         type: 'stub',
+//         click: () =>
+//           reviseDocument(
+//             document,
+//             template,
+//             attribute,
+//             this.formatFileRevision(STUB)
+//           ),
+//         title: 'Mark this file as blank'
+//       },
+//       {
+//         type: 'remove',
+//         click: () =>
+//           reviseDocument(
+//             document,
+//             template,
+//             attribute,
+//             this.formatFileRevision(null)
+//           ),
+//         title: 'Remove this file link'
+//       }
+//     ];
+
+//     return (
+//       <div className='attribute file'>
+//         <input
+//           type='file'
+//           style={{display: 'none'}}
+//           ref={(input) => (this.input = input)}
+//           onChange={this.setTempRevision}
+//         />
+//         {metis && this.metisSelector()}
+//         <ButtonBar className='file-buttons' buttons={buttons} />
+//         <FileValue value={revised_value} />
+//       </div>
+//     );
+//   }
+// }
+
+function useFileActions(metis, error, setMetis, setError, props) {
+  const invoke = useActionInvoker();
+  const metisPathRef = useRef(null);
+
+  return {
+    metisSelector,
+    closeModal,
+    selectMetisFile,
+    formatFileRevision,
+    setTempRevision
+  };
+
+  function metisSelector() {
     // TODO: would be nice to make this like a folder / file search
-    let {error} = this.state;
-
     return (
       <Modal
-        isOpen={this.state.metis}
-        contentLabel="Enter Metis path"
+        isOpen={metis}
+        contentLabel='Enter Metis path'
         style={customStyles}
-        onRequestClose={this.closeModal}
+        onRequestClose={closeModal}
         appElement={document.querySelector('#root')}
       >
-        <div className="attribute modal file-metis-select">
+        <div className='attribute modal file-metis-select'>
           <h2>Enter a Metis path</h2>
-          <div className="input-box-wrapper">
-            <label htmlFor="metis-path-input">Metis path:</label>
+          <div className='input-box-wrapper'>
+            <label htmlFor='metis-path-input'>Metis path:</label>
             <input
-              id="metis-path-input"
-              className="full_text metis-path-input"
-              type="text"
-              ref={(metis_file) => (this.metis_file = metis_file)}
-              placeholder="metis://<project>/<bucket>/<file-path>"
+              id='metis-path-input'
+              className='full_text metis-path-input'
+              type='text'
+              ref={metisPathRef}
+              placeholder='metis://<project>/<bucket>/<file-path>'
             />
-            <div className="modal-button-wrapper">
+            <div className='modal-button-wrapper'>
               <ButtonBar
-                className="modal-buttons"
+                className='modal-buttons'
                 buttons={[
-                  {type: 'check', click: () => this.selectMetisFile()},
-                  {type: 'cancel', click: () => this.setState({metis: false})}
+                  {type: 'check', click: () => selectMetisFile()},
+                  {type: 'cancel', click: () => setMetis(false)}
                 ]}
               />
               {error ? (
-                <p className="file-metis-error">Invalid Metis path</p>
+                <p className='file-metis-error'>Invalid Metis path</p>
               ) : (
                 ''
               )}
-              <div className="modal-buttons pull-right">
+              <div className='modal-buttons pull-right'>
                 <Icon
-                  className=""
-                  icon="question-circle"
-                  title="Help"
+                  className=''
+                  icon='question-circle'
+                  title='Help'
                   onClick={() =>
                     window.open(
                       'https://mountetna.github.io/timur.html#managing-data-files',
@@ -182,51 +289,52 @@ class FileAttribute extends React.Component {
     );
   }
 
-  closeModal() {
-    this.setState({metis: false});
+  function closeModal() {
+    setMetis(false);
   }
 
-  selectMetisFile() {
-    let {document, template, attribute, reviseDocument} = this.props;
-    let {value} = this.metis_file;
+  function selectMetisFile() {
+    let {document, template, attribute} = props;
 
-    if (!METIS_PATH_MATCH(value)) {
-      this.setState({error: true});
+    const metisPath = metisPathRef.current.value;
+
+    if (!METIS_PATH_MATCH(metisPath)) {
+      setError(true);
       return;
     } else {
-      this.setState({error: false, metis: false});
+      setError(false);
+      setMetis(false);
     }
 
-    this.metis_file.value = '';
-
-    reviseDocument(
-      document,
-      template,
-      attribute,
-      this.formatFileRevision(value)
+    invoke(
+      reviseDocument(
+        document,
+        template,
+        attribute,
+        formatFileRevision(metisPath)
+      )
     );
+
+    metisPathRef.current.value = null;
   }
 
-  formatFileRevision(value) {
+  function formatFileRevision(value) {
     return {path: value};
   }
 
-  getTempUrl(e) {
+  function setTempRevision(e) {
     e.preventDefault();
-    let {document, template, attribute, getRevisionTempUrl} = this.props;
-
-    getRevisionTempUrl(
-      document,
-      template,
-      attribute,
-      this.formatFileRevision(TEMP)
-    ).then((res) => {
-      console.log(res);
-      // Here upload file to Metis, then call reviseDocument
-    });
+    console.log('about to call reviseDocument');
+    let {document, template, attribute} = props;
+    invoke(
+      reviseDocument(
+        document,
+        template,
+        attribute,
+        Object.assign(formatFileRevision(TEMP), {
+          original_files: e.target.files
+        })
+      )
+    );
   }
 }
-
-export default connect(null, {reviseDocument, getRevisionTempUrl})(
-  FileAttribute
-);
