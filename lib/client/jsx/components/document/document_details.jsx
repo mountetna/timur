233eeb05004076815884386b0f12eabe24f9710b
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-01-07 09:30:18 -0800

    v 0.0.1.gvaihir fix (document_window) - sections are now in a hash table fix (document_details) - accessRadio is now optional

--- lib/client/jsx/components/document/document_details.jsx
+++ lib/client/jsx/components/document/document_details.jsx
@@ -23,33 +23,44 @@ const AccessRadio = ({onUpdate, access, value, disabled}) =>
 const DocumentDetails = ({
   document: { user, description, updated_at, access },
   onUpdate, editing
-}) => (
-  <div className='document-details'>
-    <div className='document-edit-details'>
-      <i className='fas fa-clock' title='Updated'/>
-      <span>{ formatDate(updated_at) }</span>
-      <i className='fas fa-user' title='Author'/>
-      <span>{user}</span>
+}) => {
+  let accessRadio;
+  if (access) {
+    accessRadio =
+      (<div className='document-access'>
+        <AccessRadio
+          onUpdate={onUpdate('access')}
+          disabled={!editing}
+          access={access}
+          value='private'/>
+        <AccessRadio
+          onUpdate={onUpdate('access')}
+          disabled={!editing}
+          access={access}
+          value='public'/>
+      </div>);
+  } else {
+    accessRadio = null;
+  }
+
+  return (
+    <div className='document-details'>
+      <div className='document-edit-details'>
+        <i className='fas fa-clock' title='Updated'/>
+        <span>{ formatDate(updated_at) }</span>
+        <i className='fas fa-user' title='Author'/>
+        <span>{user}</span>
+      </div>
+      {accessRadio}
+      <textarea className='document-description'
+                onChange={ onUpdate('description') }
+                value={ (description) ? description : '' }
+                placeholder='No description'
+                disabled={ !editing }
+      />
     </div>
-    <div className='document-access'>
-      <AccessRadio
-        onUpdate={ onUpdate('access') }
-        disabled={ !editing }
-        access={ access }
-        value='private'/>
-      <AccessRadio
-        onUpdate={ onUpdate('access') }
-        disabled={ !editing }
-        access={ access }
-        value='public'/>
-    </div>
-    <textarea className='document-description'
-      onChange={ onUpdate('description') }
-      value={ (description) ? description : '' }
-      placeholder='No description'
-      disabled={ !editing }
-    />
-  </div>
-);
+  );
+};
+
 
 export default DocumentDetails;
--- lib/client/jsx/components/document/document_window.jsx
+++ lib/client/jsx/components/document/document_window.jsx
@@ -3,60 +3,67 @@ import * as React from 'react';
 import DocumentView from './document_view';
 import ListMenu from '../list_menu';
 
-const accessFilter = (access, documents) => {
-  return documents.filter((m) => m.access == access).sort((a, b) => a > b);
+const groupByFeature = (documents, featureName) => {
+  return documents.reduce((res, x) => {
+  let key = x[featureName];
+  if (!res[key]) res[key] = [x];
+  else res[key].push(x);
+  return res;
+  }, {});
 };
 
-export default class DocumentWindow extends React.Component {
-  render() {
-    let {
-      documentType,
-      document,
-      documents,
-      editing,
-      children,
-      onCreate,
-      onSelect,
-      onEdit,
-      onCancel,
-      onRun,
-      onSave,
-      onCopy,
-      onRemove,
-      onUpdate
-    } = this.props;
+const DocumentWindow = (props) => {
+  let {
+    documentType,
+    document,
+    documents,
+    editing,
+    children,
+    onCreate,
+    onSelect,
+    onEdit,
+    onCancel,
+    onRun,
+    onSave,
+    onCopy,
+    onRemove,
+    onUpdate
+  } = props;
 
-    let sections = {
-      Public: accessFilter('public', documents || []),
-      Private: accessFilter('private', documents || [])
-    };
+  let groupKey;
+  if (documentType === 'view') groupKey = 'model_name';
+  else groupKey = 'access';
 
-    return (
-      <div className={`document-window ${documentType}-window`}>
-        <div className='left-column-group'>
-          <ListMenu
-            name={documentType}
-            create={onCreate}
-            select={onSelect}
-            sections={sections}
-          />
-        </div>
-        <div className='right-column-group'>
-          <DocumentView
-            editing={editing}
-            onEdit={onEdit}
-            onCancel={onCancel}
-            onRun={onRun}
-            onSave={onSave}
-            onCopy={onCopy}
-            onRemove={onRemove}
-            onUpdate={onUpdate}
-            document={document}
-          >
-            {children}
-          </DocumentView>
-        </div>
+  let sections = groupByFeature(documents || [], groupKey);
+
+  return (
+    <div className={`document-window ${documentType}-window`}>
+      <div className='left-column-group'>
+        <ListMenu
+          name={documentType}
+          create={onCreate}
+          select={onSelect}
+          sections={sections}
+        />
       </div>
-    );
-  }
-}
+      <div className='right-column-group'>
+        <DocumentView
+          editing={editing}
+          onEdit={onEdit}
+          onCancel={onCancel}
+          onRun={onRun}
+          onSave={onSave}
+          onCopy={onCopy}
+          onRemove={onRemove}
+          onUpdate={onUpdate}
+          document={document}
+        >
+          {children}
+        </DocumentView>
+      </div>
+    </div>
+  );
+
+};
+
+export default DocumentWindow;
