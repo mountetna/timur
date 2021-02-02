57691e9d51258e4147aa11e48a92cd3c3033fd49
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-02-01 17:42:25 -0800

    v 0.0.1.gvaihir feat (view_editor) - list of available views

--- lib/client/jsx/components/view_editor/view_editor.jsx
+++ lib/client/jsx/components/view_editor/view_editor.jsx
@@ -10,7 +10,6 @@ import {
 } from '../../actions/view_actions';
 import DocumentWindow from '../document/document_window';
 import ViewScript from './views_script';
-import {MD5} from '../../selectors/consignment_selector';
 
 // shallow selector
 export const useShallowEqualSelector = (selector) => {
@@ -19,16 +18,10 @@ export const useShallowEqualSelector = (selector) => {
 
 // views to the similar format as other documents
 // TODO refactor because generates obscure keys 'list-undefined'
-const unpackViews = (viewsFromStore) => {
-	return viewsFromStore.reduce((allViews, view) => {
-		for (let tab of view.document.tabs) {
-			tab['model_name'] = view.model_name;
-			let newName = [view.model_name, tab.name].join('_');
-			tab['id'] = newName;
-			allViews[newName] = tab;
-		}
-		return allViews;
-	}, {});
+const addViewName = (viewsFromStore) => {
+	return (
+		viewsFromStore.map((view) => ({...view, name: view['model_name']}))
+	);
 };
 
 // Main component for viewing/editing views.
@@ -43,19 +36,31 @@ const ViewEditor = (props) => {
 		})(dispatch);
 	}, []);
 	let viewsFromStore = useSelector(state => Object.values(state.views));
-	let views = unpackViews(viewsFromStore);
+	let views = addViewName(viewsFromStore);
 	let [view, setView] = useState(useShallowEqualSelector(state => state.view));
 	console.log(views)
 
 
+
 	const selectView = (id, push = true) => {
 		switch (id) {
+			case 'new':
+				let date = new Date();
+				view = {
+					id: 0,
+					name: '',
+					description: '',
+					script: '',
+					created_at: date.toString(),
+					updated_at: date.toString()
+				};
+				break;
 			case null:
 				view = setView(null);
 				break;
 			default:
 				// find it in the existing views
-				view = setView(views && views.id);
+				view = setView(views && views.name);
 				if (!view) return;
 
 				// copy it so you don't modify the store
@@ -64,19 +69,21 @@ const ViewEditor = (props) => {
 				break;
 		}
 		setView({...view});
+		setEditing(id === 'new');
 
 		if (push)
 			pushLocation(
 				id == null
-					? Routes.fetch_view_path(TIMUR_CONFIG.project_name)
-					: Routes.view_path(TIMUR_CONFIG.project_name, id)
+					? Routes.views_path(CONFIG.project_name)
+					: Routes.curr_view_path(CONFIG.project_name, id)
 			)(dispatch);
-
 	};
 
 	const activateView = (view) => {
 		selectView(view.id);
 	};
+
+	const create = () => selectView('new', true);
 
 	const updateField = (field_name) => (event) => {
 		if (field_name === 'script') {
@@ -110,8 +117,8 @@ const ViewEditor = (props) => {
 			documentType='view'
 			editing={editing}
 			document={view}
-			documents={Object.values(views)}
-			//onCreate={create}
+			documents={views}
+			onCreate={create}
 			onSelect={activateView}
 			onUpdate={updateField}
 			onEdit={toggleEdit}
