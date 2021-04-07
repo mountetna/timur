d1b99aaa66efa4c242565883777710baee605dfb
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2021-04-07 09:03:26 -0700

    allow multicolumn format for panes

--- lib/client/jsx/components/browser/view_tab.jsx
+++ lib/client/jsx/components/browser/view_tab.jsx
@@ -4,19 +4,34 @@ import * as React from 'react';
 // Class imports.
 import ViewPane from './view_pane';
 
-const ViewTab = ({ tab, ...pane_props }) =>
-  tab ?
-    <div id='tab'>
-      {
-        tab.panes.map(pane=>
-          <ViewPane
-            { ...pane_props }
-            pane={ pane }
-            key={ pane.name }
-          />
-        )
-      }
-    </div>
-  : <span className='fas fa-spinner fa-pulse' />;
+const ViewTab = ({ tab, ...pane_props }) => {
+  if (!tab) return <span className='fas fa-spinner fa-pulse' />;
+
+  let tab_groups = tab.panes.reduce(
+    (groups, pane) => {
+      groups[pane.group] = groups[pane.group] || [];
+      groups[pane.group].push(pane);
+      return groups;
+    }, {}
+  );
+  console.log({tab_groups});
+  return <div id='tab'>
+    {
+      Object.keys(tab_groups).map((pane_group,i) =>
+        <div className='pane_group' key={i}>
+          {
+            tab_groups[pane_group].map( pane =>
+              <ViewPane
+                { ...pane_props }
+                pane={ pane }
+                key={ pane.name }
+              />
+            )
+          }
+        </div>
+      )
+    }
+  </div>
+}
 
 export default ViewTab;
