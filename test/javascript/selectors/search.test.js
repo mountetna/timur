735ae27b7426f0ae30f20474a102cb6766ff01cc
Cole <4930129+coleshaw@users.noreply.github.com>
2021-04-23 15:41:07 -0400

    update ui specs

--- test/javascript/selectors/search.test.js
+++ test/javascript/selectors/search.test.js
@@ -1,6 +1,5 @@
 import {
   selectSearchAttributeNames,
-  selectSearchFilterParams,
   selectSearchFilterString,
   selectSortedDisplayAttributeNames
 } from '../../../lib/client/jsx/selectors/search';
@@ -25,29 +24,11 @@ describe('selectSearchAttributeNames', () => {
   });
 });
 
-describe('selectSearchFilterParams', () => {
-  it('returns the filter_params from the search state', () => {
-    let state = {
-      search: {
-        attribute_names: ['name', 'stats', 'species'],
-        filter_params: [{attribute: 'species', operator: '=', value: 'lion'}]
-      }
-    };
-
-    let filter_params = selectSearchFilterParams(state);
-
-    expect(filter_params).toEqual([
-      {attribute: 'species', operator: '=', value: 'lion'}
-    ]);
-  });
-});
-
 describe('selectSearchFilterString', () => {
   it('returns the filter_string from the search state', () => {
     let state = {
       search: {
         attribute_names: ['name', 'stats', 'species'],
-        filter_params: [{attribute: 'species', operator: '=', value: 'lion'}],
         filter_string: 'all'
       }
     };
