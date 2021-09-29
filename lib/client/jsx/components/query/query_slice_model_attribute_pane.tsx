1748ff948e3e2e06c2ff3f6a840cef82753f6b9c
Cole <4930129+coleshaw@users.noreply.github.com>
2021-09-29 16:06:20 -0400

    tweaks

--- lib/client/jsx/components/query/query_slice_model_attribute_pane.tsx
+++ lib/client/jsx/components/query/query_slice_model_attribute_pane.tsx
@@ -3,11 +3,24 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import Tooltip from '@material-ui/core/Tooltip';
+import Paper from '@material-ui/core/Paper';
+
+import {makeStyles} from '@material-ui/core/styles';
 
 import {QueryGraphContext} from '../../contexts/query/query_graph_context';
 import QueryFilterControl from './query_filter_control';
 import {QuerySlice, QueryColumn} from '../../contexts/query/query_types';
 import useSliceMethods from './query_use_slice_methods';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: '0 0.5rem',
+    marginBottom: '0.5rem'
+  },
+  grid: {
+    paddingTop: '0.5rem'
+  }
+}));
 
 const QuerySliceModelAttributePane = ({
   column,
@@ -23,6 +36,7 @@ const QuerySliceModelAttributePane = ({
   const {
     state: {graph, rootModel}
   } = useContext(QueryGraphContext);
+  const classes = useStyles();
 
   const {matrixModelNames, addNewSlice, handlePatchSlice, handleRemoveSlice} =
     useSliceMethods(columnIndex, updateCounter, setUpdateCounter);
@@ -53,19 +67,23 @@ const QuerySliceModelAttributePane = ({
           Add slice
         </Button>
       </Tooltip>
-      <Grid container spacing={1}>
+      <Grid container direction='column' className={classes.grid}>
         {column?.slices.map((slice: QuerySlice, index: number) => (
-          <QueryFilterControl
-            key={`model-${column.model_name}-${index}-${updateCounter}`}
-            filter={slice}
-            isColumnFilter={true}
-            modelNames={sliceableModelNames}
-            graph={graph}
-            patchFilter={(updatedFilter: QuerySlice) =>
-              handlePatchSlice(index, updatedFilter)
-            }
-            removeFilter={() => handleRemoveSlice(index)}
-          />
+          <Paper className={classes.paper}>
+            <Grid container spacing={1} alignItems='center'>
+              <QueryFilterControl
+                key={`model-${column.model_name}-${index}-${updateCounter}`}
+                filter={slice}
+                isColumnFilter={true}
+                modelNames={sliceableModelNames}
+                graph={graph}
+                patchFilter={(updatedFilter: QuerySlice) =>
+                  handlePatchSlice(index, updatedFilter)
+                }
+                removeFilter={() => handleRemoveSlice(index)}
+              />
+            </Grid>
+          </Paper>
         ))}
       </Grid>
     </React.Fragment>
