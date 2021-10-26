c40ab6389029a51bcd2b22434df793019f48e9da
Cole <4930129+coleshaw@users.noreply.github.com>
2021-10-26 11:27:55 -0400

    refactor

--- lib/client/jsx/components/attributes/shifted_date_time_attribute.jsx
+++ lib/client/jsx/components/attributes/shifted_date_time_attribute.jsx
@@ -6,11 +6,12 @@ import {useReduxState} from 'etna-js/hooks/useReduxState';
 import {selectUser} from 'etna-js/selectors/user-selector';
 import {isPrivileged} from 'etna-js/utils/janus';
 
-const ShiftedDateTimeAttribute = ({mode, value, ...props}) => {
+const ShiftedDateTimeAttribute = (props) => {
   const user = useReduxState((state) => selectUser(state));
+  const {mode, value} = props;
 
   if (mode == 'edit' && isPrivileged(user, CONFIG.project_name))
-    return <DateTimeAttribute {...props} mode={mode} value={value} />;
+    return <DateTimeAttribute {...props} />;
 
   return <div className='attribute'>{formatDate(value)}</div>;
 };
