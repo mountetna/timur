d0f9dd8b3af0da128521f64afa1343e5a4678838
Saurabh Asthana <faplap@gmail.com>
2022-04-20 09:43:30 -0700

    fix tests

--- test/javascript/components/attribute_report.test.js
+++ test/javascript/components/attribute_report.test.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {mockStore} from '../helpers';
 import renderer from 'react-test-renderer';
 import AttributeReport from '../../../lib/client/jsx/components/model_map/attribute_report';
 
 const monster = require('../fixtures/template_monster.json');
 
 describe('AttributeReport', () => {
+  const store = mockStore({ });
+
   it('renders', () => {
     const tree = renderer
-      .create(<AttributeReport attribute={monster.attributes.name} />)
+    .create(<Provider store={store}>
+      <AttributeReport attribute={monster.attributes.name} />
+      </Provider>)
       .toJSON();
 
     expect(tree).toMatchSnapshot();
