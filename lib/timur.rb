eb6e70004ded16d7b0d0902285b2661b0fdb9a0b
Zachary Collins <recursive.cookie.jar@gmail.com>
2022-06-16 14:24:40 -0700

    Fixing timur migration issue

--- lib/timur.rb
+++ lib/timur.rb
@@ -4,12 +4,12 @@ class Timur
   include Etna::Application
   attr_reader :db
 
-  def setup_db
+  def setup_db(load_models = true)
     @db = Sequel.connect(config(:db))
     @db.extension :connection_validator
     @db.extension :pg_json
     @db.pool.connection_validation_timeout = -1
 
-    require_relative 'models'
+    require_relative 'models' if load_models
   end
 end
